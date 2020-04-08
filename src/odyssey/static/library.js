b9e4b5a4c27e24d90f61db0d5f150bfbe6c535fb
Zan Peeters <zan.peeters@atventurepartners.tech>
2020-04-08 12:02:28 -0700

    Remove unused javascript functions.

--- src/odyssey/static/library.js
+++ src/odyssey/static/library.js
@@ -1,24 +1,3 @@
-function toggle(target_class, id) {
-    var divs = document.querySelectorAll(target_class);
-    divs.forEach(div => {
-        if (div.id == id && div.style.display == "none") {
-            div.style.display = "inline-block";
-        } else {
-            div.style.display = "none";
-        }
-    });
-};
-
-function add_toggles(listen_class, target_class) {
-    var divs = document.querySelectorAll(listen_class);
-    divs.forEach(div => {
-        div.addEventListener("click", function(){
-            var target_id = this.getAttribute('data-id');
-            toggle(target_class, target_id);
-        });
-    });
-};
-
 function add_signature_pad(submit_button, signature_field) {
     var canvas = document.querySelector("canvas");
     var pad = new SignaturePad(canvas);
@@ -38,10 +17,10 @@ function age(birthday) {
     var now = new Date();
     var bday = new Date(birthday);
     // Born in the future
-    if (now < bday) {
+    if (now <= bday) {
         return 0
     }
     var age_diff = now - bday.getTime();                // in millisec
-    var age_date = new Date(age_diff);                  // milisec from epoch
+    var age_date = new Date(age_diff);                  // millisec from epoch
     return Math.abs(age_date.getUTCFullYear() - 1970);
 }
