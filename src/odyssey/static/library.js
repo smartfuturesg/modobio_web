3e3deec74ebebf404f457b8936ce294a357324a2
Zan Peeters <zan.peeters@atventurepartners.tech>
2020-04-10 18:34:29 -0700

    Make signature pad load existing signatures, make clear button work, and make it scale on high DPI screens.

--- src/odyssey/static/library.js
+++ src/odyssey/static/library.js
@@ -1,16 +1,31 @@
-function add_signature_pad(submit_button, signature_field) {
-    var canvas = document.querySelector("canvas");
+function add_signature_pad(canvas_id, form_id, clear_button_id, signature_field_id) {
+    var canvas = document.getElementById(canvas_id);
+    var form = document.getElementById(form_id);
+    var clear  = document.getElementById(clear_button_id);
+    var sigbox = document.getElementById(signature_field_id);
     var pad = new SignaturePad(canvas);
-    var submit = document.getElementById(submit_button);
-    var sigbox = document.getElementById(signature_field);
-    submit.addEventListener("click", function() {
+
+    var ratio =  Math.max(window.devicePixelRatio || 1, 1);
+    canvas.width = canvas.offsetWidth * ratio;
+    canvas.height = canvas.offsetHeight * ratio;
+    canvas.getContext("2d").scale(ratio, ratio);
+
+    if (sigbox.value != "") {
+        pad.fromDataURL(sigbox.value);
+    }
+
+    form.onsubmit = function() {
         if (pad.isEmpty()) {
             alert("Please sign the document before clicking Accept.");
             return false
         } else {
             sigbox.value = pad.toDataURL();
         }
-    })
+    }
+
+    clear.addEventListener("click", function() {
+        pad.clear();
+    });
 }
 
 function age(birthday) {
