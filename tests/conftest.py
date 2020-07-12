ff115d97f4113a43039c2660be2e98ad4c366a2b
Zan Peeters <zan.peeters@atventurepartners.tech>
2020-07-11 19:43:18 -0700

    Functional testing of the client consent form database table, API, and PDF generation. Of all the signable documents, only consent form implemented so far.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,28 @@
-from odyssey import create_app, db
-from odyssey.models.intake import ClientInfo
-from odyssey.models.main import Staff
-import pytest 
+import pytest
 
-from .data import test_client_info, test_staff_member
+from odyssey import create_app, db
+from odyssey.models.intake import (
+    ClientInfo,
+    ClientConsent,
+    ClientRelease,
+    ClientPolicies,
+    ClientConsultContract,
+    ClientSubscriptionContract,
+    ClientIndividualContract
+)
+
+from odyssey.models.main import Staff
+
+from .data import (
+    test_client_info,
+    test_staff_member,
+    test_client_consent_form,
+    test_client_release_form,
+    test_client_policies_form,
+    test_client_consult_contract,
+    test_client_subscription_contract,
+    test_client_individual_contract
+)
 
 @pytest.fixture(scope='module')
 def new_client():
@@ -37,12 +56,33 @@ def init_database():
     client_1 = ClientInfo()
     client_1.from_dict(test_client_info)
     db.session.add(client_1)
+    db.session.commit()
+    clientid = client_1.clientid
 
     # initialize a test staff member
     staff_1 = Staff()
     staff_1.from_dict(test_staff_member)
     staff_1.set_password(test_staff_member['password'])
     db.session.add(staff_1)
+
+    # Populate document tables
+    consent_1 = ClientConsent(clientid=clientid, **test_client_consent_form)
+    db.session.add(consent_1)
+
+    release_1 = ClientRelease(clientid=clientid, **test_client_release_form)
+    db.session.add(consent_1)
+
+    policies_1 = ClientPolicies(clientid=clientid, **test_client_policies_form)
+    db.session.add(policies_1)
+
+    consult_1 = ClientConsultContract(clientid=clientid, **test_client_consult_contract)
+    db.session.add(consult_1)
+
+    subscription_1 = ClientSubscriptionContract(clientid=clientid, **test_client_subscription_contract)
+    db.session.add(subscription_1)
+
+    individual_1 = ClientIndividualContract(clientid=clientid, **test_client_individual_contract)
+    db.session.add(individual_1)
 
     # Commit the changes for the users
     db.session.commit()
