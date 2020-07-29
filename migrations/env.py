fd75086d406d63b05b0b2393f7235409ff780544
Alejandro Lorenzo <alejandro.lorenzo@atventurepartners.tech>
2020-07-29 11:46:54 -0700

    only change db users if editing remote dbs

--- migrations/env.py
+++ migrations/env.py
@@ -40,12 +40,16 @@ target_metadata = current_app.extensions['migrate'].db.metadata
 # NOTE: from_pyfile() looks in odyssey root dir
 current_app.config.from_pyfile('config.py')
 
+
 # change the database user to the master user for the purpose of editing schemas
-ssm = boto3.client('ssm')
-db_flav = ssm.get_parameter(Name='/modobio/odyssey/db_flav')['Parameter']['Value']
-db_user = ssm.get_parameter(Name='/modobio/odyssey/db_user_master')['Parameter']['Value']
-db_pass = ssm.get_parameter(Name='/modobio/odyssey/db_pass_master', WithDecryption=True)['Parameter']['Value']
-db_host = ssm.get_parameter(Name='/modobio/odyssey/db_host')['Parameter']['Value']
+# this only needs to happen if we are editing a remote db
+if os.getenv('FLASK_ENV') in ('development','production') and os.getenv('FLASK_DEV') != 'local':
+    ssm = boto3.client('ssm')
+    db_flav = ssm.get_parameter(Name='/modobio/odyssey/db_flav')['Parameter']['Value']
+    db_user = ssm.get_parameter(Name='/modobio/odyssey/db_user_master')['Parameter']['Value']
+    db_pass = ssm.get_parameter(Name='/modobio/odyssey/db_pass_master', WithDecryption=True)['Parameter']['Value']
+    db_host = ssm.get_parameter(Name='/modobio/odyssey/db_host')['Parameter']['Value']
+
 if os.getenv('FLASK_ENV') == 'development':
     if os.getenv('FLASK_DEV') == 'local':
         db_connection_string = str(current_app.extensions['migrate'].db.engine.url).replace('%', '%%')
