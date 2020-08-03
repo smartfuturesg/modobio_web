932a2d273a87165a77297d1b8d651f628502995c
Zan Peeters <zan.peeters@atventurepartners.tech>
2020-08-03 14:49:04 -0700

    Update documentation.

--- public/_static/documentation_options.js
+++ public/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.1.dev122+g33a3efc.d20200731',
+    VERSION: '0.1.1.dev132+g1a99585',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
