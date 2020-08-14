64ad2a78a10b530857bb852f794889efd220a1c1
Alejandro Lorenzo <alejandro.lorenzo@atventurepartners.tech>
2020-08-14 14:37:32 -0700

    misc.py script for misc models. Currently for medical institutes table

---
+++ src/odyssey/models/misc.py
@@ -0,0 +1,27 @@
+"""
+Database tables sor supporting miscellaneous funcitonality. 
+"""
+
+from odyssey import db
+
+
+class MedicalInstitutions(db.Model):
+    """ Medical institutions associated with client external medical records. 
+    """
+
+    __tablename__ = 'MedicalInstitutions'
+
+    institute_id = db.Column(db.Integer, primary_key=True, autoincrement=True )
+    """
+    medical institute id 
+
+    :type: int, primary key, autoincrement
+    """
+
+    institute_name = db.Column(db.String, nullable=False, unique=True)
+    """
+    medical institution name
+
+    :type: str
+    """
+
