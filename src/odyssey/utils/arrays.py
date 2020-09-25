fb8c5af20fb4160a73d0d9533265c40bd8c93da2
Zan Peeters <zan.peeters@atventurepartners.tech>
2020-09-25 11:25:37 -0700

    Subclass ARRAY

--- src/odyssey/utils/arrays.py
+++ src/odyssey/utils/arrays.py
@@ -1,3 +1,13 @@
 # Array manipulation
 
+from sqlalchemy.dialects.postgres import ARRAY
 
+
+class Array(ARRAY):
+    """ Postgres ARRAY type with extra functionality. """
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def append(self, arr: list):
+        pass
