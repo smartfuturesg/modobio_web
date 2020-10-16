5d43e97ac4661428e2c9a75209256980ea4e6f1f
dalton.harenza <dalton.harenza@atlanticventurepartners.tech>
2020-10-16 14:40:28 -0700

    update staff tests to use user_id

--- tests/00_unit/staff_test.py
+++ tests/00_unit/staff_test.py
@@ -1,5 +1,5 @@
 
-from odyssey.models.staff import Staff
+from odyssey.models.user import User, UserLogin
 from tests.data import test_staff_member
 
 def test_regular_staff_member(test_client, init_database):
@@ -8,19 +8,19 @@ def test_regular_staff_member(test_client, init_database):
     WHEN a new staff is created
     THEN check the emai
     """
-    staff = Staff().query.first()
-    user = User.query.filter_by(user_id=staff.user_id)
+    user = User.query.filter_by(is_staff=True).first()
+    user_login = UserLogin.query.filter_by(user_id=staff.user_id).one_or_none()
 
-    assert staff.email == 'staff_member@modobio.com'
-    assert staff.check_password(test_staff_member['password'])
-    assert type(user.get_token()) == str 
-    assert user.check_token(user.token)
+    assert user.email == 'staff_member@modobio.com'
+    assert user_login.check_password(test_staff_member['password'])
+    assert type(user_login.get_token()) == str 
+    assert user_login.check_token(user.token)
 
     # revoke token and then check it's validity    
-    user.revoke_token()
-    assert not user.check_token(user.token)
+    user_login.revoke_token()
+    assert not user_login.check_token(user.token)
 
     # make sure new tokens are different from old ones
-    old_token = user.token
-    user.get_token()
-    assert old_token != user.token
+    old_token = user_login.token
+    user_login.get_token()
+    assert old_token != user_login.token
--- tests/01_functional/4000_staff_search_test.py
+++ tests/01_functional/4000_staff_search_test.py
@@ -16,15 +16,15 @@ def test_get_staff_search(test_client, init_database):
     headers = {'Authorization': f'Bearer {token}'}
     
     # Order of test search:
-    # send staffid
+    # send staff user_id
     # firstname
     # lastname
     # email
     # firstname and email
-    # FORCE 404 staffid = 2 
+    # FORCE 404 user_id = 2 
 
-    # send get request for staff staffid = 1
-    response = test_client.get('/staff/?staffid=1', headers=headers)
+    # send get request for staff user_id = 1
+    response = test_client.get('/staff/?user_id=1', headers=headers)
     assert response.status_code == 200
 
     # send get request for first name (note, the actual first name is testY)
@@ -33,29 +33,29 @@ def test_get_staff_search(test_client, init_database):
     assert response.json[0]['firstname'] == 'testy'
     assert response.json[0]['lastname'] == 'testerson'
     assert response.json[0]['email'] == 'staff_member@modobio.com'
-    assert response.json[0]['staffid'] == 1
+    assert response.json[0]['user_id'] == 1
 
     # send get request for last name
     response = test_client.get('/staff/?lastname=test', headers=headers)
     assert response.json[0]['firstname'] == 'testy'
     assert response.json[0]['lastname'] == 'testerson'
     assert response.json[0]['email'] == 'staff_member@modobio.com'
-    assert response.json[0]['staffid'] == 1
+    assert response.json[0]['user_id'] == 1
     
     # send get request for email
     response = test_client.get('/staff/?email=staff_member@modobio.com', headers=headers)
     assert response.json[0]['firstname'] == 'testy'
     assert response.json[0]['lastname'] == 'testerson'
     assert response.json[0]['email'] == 'staff_member@modobio.com'
-    assert response.json[0]['staffid'] == 1
+    assert response.json[0]['user_id'] == 1
 
     # send get request for first name (note, the actual first name is testY)
     response = test_client.get('/staff/?firstname=test&email=staff_member@modobio.com', headers=headers)
     assert response.json[0]['firstname'] == 'testy'
     assert response.json[0]['lastname'] == 'testerson'
     assert response.json[0]['email'] == 'staff_member@modobio.com'
-    assert response.json[0]['staffid'] == 1
+    assert response.json[0]['user_id'] == 1
 
     # Expect response to fail because there is only TWO staff in the db
-    response = test_client.get('/staff/?staffid=99', headers=headers)
+    response = test_client.get('/staff/?user_id=99', headers=headers)
     assert response.status_code == 404
