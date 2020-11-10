20e7da24fcc3d3d016f9ea6ad91d9191a4c8ca3d
alejandro <alejandro.lorenzo@atventurepartners.tech>
2020-11-10 13:49:46 -0600

    add staff token login to unit tests

--- tests/01_functional/0020_new_staff_test.py
+++ tests/01_functional/0020_new_staff_test.py
@@ -1,3 +1,5 @@
+import base64
+
 from flask.json import dumps
 from requests.auth import _basic_auth_str
 
@@ -26,11 +28,26 @@ def test_creating_new_staff(test_client, init_database):
                                 data=dumps(test_new_user_staff), 
                                 content_type='application/json')
     
-    #the new staff member should be in the StaffProfile database 
-    staff = StaffProfile.query.filter_by(user_id=1).one_or_none()
     # some simple checks for validity
     assert response.status_code == 201
+
+    ###
+    # Login (get token) for newly created staff member
+    ##
+
+    valid_credentials = base64.b64encode(
+        f"{test_new_user_staff['userinfo']['email']}:{test_new_user_staff['userinfo']['password']}".encode(
+            "utf-8")).decode("utf-8")
     
+    headers = {'Authorization': f'Basic {valid_credentials}'}
+    response = test_client.post('/tokens/staff/',
+                            headers=headers, 
+                            content_type='application/json')
+    
+    roles = response.get_json()['access_roles']
+    assert response.status_code == 201
+    assert roles.sort() == test_new_user_staff['staffinfo']['access_roles'].sort()
+
 def test_creating_new_staff_same_email(test_client, init_database):
     """
     GIVEN a api end point for creating a new staff 
