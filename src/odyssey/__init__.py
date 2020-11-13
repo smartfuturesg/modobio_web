4eda7b6bd12c526f5f970ed2bc1f17b42d70e13c
dalton.harenza <dalton.harenza@atlanticventurepartners.tech>
2020-11-13 09:20:25 -0700

    update sqlalchemy commit behavior

--- src/odyssey/__init__.py
+++ src/odyssey/__init__.py
@@ -14,7 +14,7 @@ from flask_whooshee import Whooshee
 
 from odyssey.config import Config
 
-db = SQLAlchemy()
+db = SQLAlchemy(session_options={'expire_on_commit': False})
 migrate = Migrate()
 cors = CORS()
 ma = Marshmallow()
