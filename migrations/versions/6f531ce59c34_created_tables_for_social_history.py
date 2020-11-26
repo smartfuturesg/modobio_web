7a72f857fd3f43b42f3ca5a30ef1b03e87e3605b
Steve.Thach <steve@AVPC014.localdomain>
2020-11-26 14:04:37 -0700

    linearized migrations

--- migrations/versions/6f531ce59c34_created_tables_for_social_history.py
+++ migrations/versions/6f531ce59c34_created_tables_for_social_history.py
@@ -11,7 +11,7 @@ from sqlalchemy.dialects import postgresql
 
 # revision identifiers, used by Alembic.
 revision = '6f531ce59c34'
-down_revision = '65f48672909f'
+down_revision = 'e2b7f6857eaf'
 branch_labels = None
 depends_on = None
 
