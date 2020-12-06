b210988a38f6db79e4eeca1b2da86a9cfa43fcf2
alejandro <alejandro.lorenzo@atventurepartners.tech>
2020-12-06 09:58:07 -0700

    commit user context at portal_id verification

--- src/odyssey/api/user/routes.py
+++ src/odyssey/api/user/routes.py
@@ -289,4 +289,6 @@ class VerifyPortalId(Resource):
         elif decoded_token['utype'] == 'staff':
             user.is_staff = True
         
+        db.session.commit()
+        
         return 200
