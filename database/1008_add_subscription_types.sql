b1a6ab81952f2c2f68b252d0b7d443b9def949c1
dalton.harenza <dalton.harenza@atlanticventurepartners.tech>
2021-01-21 11:28:39 -0700

    add subscription lookup data

---
+++ database/1008_add_subscription_types.sql
@@ -0,0 +1,9 @@
+delete from "LookupSubscriptions" 
+			where sub_id >= 1;
+alter sequence "LookupSubscriptions_sub_id_seq"
+  restart with 1;
+
+INSERT INTO "LookupSubscriptions" ("name", "description", "cost", "frequency") 
+VALUES
+('Unsubscribed', 'Join a clinical care team!', 0.00, 'Monthly'),
+('Subsribed', 'Make Telehealth Appointments\nHave Activity Tracker Data Analysed\nCreate a Clinical Care Team', 10.00, 'Monthly');
