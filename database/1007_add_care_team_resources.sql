1ccd4a333cb7db87f13a3c85369f3a06b698b469
ALEJANDRO <alejandro.lorenzo@atlanticventurepartners.tech>
2021-01-20 22:30:40 -0700

    clinical care team migration

---
+++ database/1007_add_care_team_resources.sql
@@ -0,0 +1,14 @@
+delete from "LookupClinicalCareTeamResources" 
+			where resource_id >= 1;
+alter sequence "LookupClinicalCareTeamResources_resource_id_seq"
+  restart with 1;
+
+INSERT INTO "LookupClinicalCareTeamResources" ("resource_name", "display_name") 
+VALUES
+('MedicalBloodTestResults', 'Blood Tests'),
+('MedicalConditions', 'Medical Conditions'),
+('MedicalGeneralInfo', 'General Medical Information'),
+('MedicalGeneralInfoMedications', 'Medications'),
+('MedicalImaging', 'Medical Images'),
+('MedicalSTDHistory', 'STD History')
+('MedicalSocialHistory', 'Social History');
