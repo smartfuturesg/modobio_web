2437e2703d2600ebba663d4b6b1ef870a84750bd
dalton.harenza <dalton.harenza@atlanticventurepartners.tech>
2021-02-10 13:55:21 -0700

    add missing schema information

--- src/odyssey/api/system/schemas.py
+++ src/odyssey/api/system/schemas.py
@@ -8,17 +8,21 @@ from odyssey.utils.constants import ACCESS_ROLES
 class SystemTeleheathCostSchema(ma.SQLAlchemyAutoSchema):
     class Meta:
         model = SystemTelehealthSessionCosts
+        exclude = ('created_at', 'updated_at')
 
     profession_type = fields.String(validate=validate.OneOf(ACCESS_ROLES))
+    territory_name = fields.String(dump_only=True)
+    territory_id = fields.Integer()
 
     @post_load
     def make_object(self, data, **kwargs):
-        return SystemTelehealthSettings(**data)
+        return SystemTelehealthSessionCosts(**data)
 
 class SystemTelehealthSettingsSchema(Schema):
     
-    costs = fields.Nested(SystemTeleheathCostSchema,
-                            missing={}, 
+    costs = fields.Nested(SystemTeleheathCostSchema(many=True),
+                            missing={},
                             description="Used when changing session costs for professions/territories")
-    session_duration = fields.Integer()
-    booking_notice_window = fields.Integer()
+    session_duration = fields.Integer(validate=validate.Range(min=10, max=60))
+    booking_notice_window = fields.Integer(validate=validate.Range(min=8, max=24))
+    confirmation_window = fields.Float(validate=validate.Range(min=1.0, max=24.0))
