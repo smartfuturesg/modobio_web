6e853f59202e73291faacf7a7d4fff124edfa5c1
dalton.harenza <dalton.harenza@atlanticventurepartners.tech>
2021-03-01 08:28:15 -0700

    update tests, add migration

--- database/1009_add_telehealth_session_cost.sql
+++ database/1009_add_telehealth_session_cost.sql
@@ -3,7 +3,7 @@ delete from "LookupTelehealthSessionCost"
 alter sequence "LookupTelehealthSessionCost_idx_seq"
   restart with 1;
 
-INSERT INTO "LookupTelehealthSessionCost" ("profession_type","territory","session_cost") 
+INSERT INTO "LookupTelehealthSessionCost" ("profession_type","territory","session_min_cost","session_max_cost") 
 VALUES
 ('Medical Doctor', 'USA', 50.00, 200.00),
 ('Medical Doctor', 'UK', 50.00, 200.00);
