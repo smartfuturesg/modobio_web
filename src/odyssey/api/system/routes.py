d120598193792b0060b7430f2af8be0a67bdbaef
dalton.harenza <dalton.harenza@atlanticventurepartners.tech>
2021-03-05 10:24:48 -0700

    move telehealth variables to sql

--- src/odyssey/api/system/routes.py
+++ src/odyssey/api/system/routes.py
@@ -3,10 +3,9 @@ from flask_accepts import responds, accepts
 from flask_restx import Resource
 
 from odyssey.api import api
-from odyssey.defaults import TELEHEALTH_BOOKING_NOTICE_WINDOW, TELEHEALTH_SESSION_DURATION, TELEHEALTH_CONFIRMATION_WINDOW
 
 from odyssey.utils.auth import token_auth
-from odyssey.api.system.models import SystemTelehealthSessionCosts
+from odyssey.api.system.models import SystemTelehealthSessionCosts, SystemVariables
 from odyssey.api.system.schemas import SystemTelehealthSettingsSchema
 from odyssey.api.lookup.models import LookupCountriesOfOperations
 
@@ -20,20 +19,24 @@ class SystemTelehealthSettingsApi(Resource):
     """ Endpoints related to system telehealth settings.
     """
 
-    @token_auth.login_required(user_type=('staff',), staff_role=('system_admin',))
+    #@token_auth.login_required(user_type=('staff',), staff_role=('system_admin',))
     @responds(schema=SystemTelehealthSettingsSchema,status_code=200, api=ns)
     def get(self):
         costs = SystemTelehealthSessionCosts.query.all()
         for cost in costs:
             cost.territory_name = LookupCountriesOfOperations.query.filter_by(idx=cost.territory_id).one_or_none().country
 
+        session_duration = int(SystemVariables.query.filter_by(var_name='Session Duration').one_or_none().var_value)
+        booking_notice_window = int(SystemVariables.query.filter_by(var_name='Booking Notice Window').one_or_none().var_value)
+        confirmation_window = float(SystemVariables.query.filter_by(var_name='Confirmation Window').one_or_none().var_value)
+
         res = {'costs': costs,
-                'session_duration': TELEHEALTH_SESSION_DURATION,
-                'booking_notice_window': TELEHEALTH_BOOKING_NOTICE_WINDOW,
-                'confirmation_window': TELEHEALTH_CONFIRMATION_WINDOW}
+                'session_duration': session_duration,
+                'booking_notice_window': booking_notice_window,
+                'confirmation_window': confirmation_window}
         return res
 
-    @token_auth.login_required(user_type=('staff',), staff_role=('system_admin',))
+    #@token_auth.login_required(user_type=('staff',), staff_role=('system_admin',))
     @accepts(schema=SystemTelehealthSettingsSchema, api=ns)
     @responds(schema=SystemTelehealthSettingsSchema, status_code=201, api=ns)
     def put(self):
@@ -48,14 +51,16 @@ class SystemTelehealthSettingsApi(Resource):
             else:
                 db.session.add(cost)
                 
-        db.session.commit()
-
         #update session variables
         if 'session_duration' in request.parsed_obj:
-            TELEHEALTH_SESSION_DURATION = request.parsed_obj['session_duration']
+            ses_dur = SystemVariables.query.filter_by(var_name='Session Duration').one_or_none()
+            ses_dur.update({'var_value': str(request.parsed_obj['session_duration'])})
         if 'booking_notice_window' in request.parsed_obj:
-            TELEHEALTH_BOOKING_NOTICE_WINDOW = request.parsed_obj['booking_notice_window']
+            book_window = SystemVariables.query.filter_by(var_name='Booking Notice Window').one_or_none()
+            book_window.update({'var_value': str(request.parsed_obj['booking_notice_window'])})
         if 'confirmation_window' in request.parsed_obj:
-            TELEHEALTH_CONFIRMATION_WINDOW = request.parsed_obj['confirmation_window']
+            con_window = SystemVariables.query.filter_by(var_name='Confirmation Window').one_or_none()
+            con_window.update({'var_value': str(request.parsed_obj['confirmation_window'])})
 
+        db.session.commit()
         return request.parsed_obj
