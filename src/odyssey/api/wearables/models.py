40b10c42b77ee96c7c46de0acb68eeb7ce896279
Zan Peeters <zan.peeters@atventurepartners.tech>
2021-04-13 12:22:01 -0700

    One-to-one relationship on wearables tables.

--- src/odyssey/api/wearables/models.py
+++ src/odyssey/api/wearables/models.py
@@ -1,31 +1,17 @@
 """
 Database tables for the wearable devices section of the Modo Bio Staff application.
 All tables in this module are prefixed with 'Wearables'.
-
-The models in this file are shared between the Odyssey and the Wearables programs.
-They describe tables in the ``modobio`` database.
 """
 
 import sys
 
-# If the Flask module is imported, assume we are running in a flask app,
-# and therefore we are part of Odyssey.
-if 'flask' in sys.modules:
-    from odyssey import db
-    _Base = db.Model
-else:
-    # In the Wearables program, Alembic should not try to update these models
-    # in the database. We separate it by giving these models a different base
-    # from the rest of the Wearables program.
-    from sqlalchemy.ext.declarative import declarative_base
-    _Base = declarative_base()
-
 from sqlalchemy import Boolean, Column, DateTime, Float, ForeignKey, Integer, String
 from sqlalchemy.dialects.postgresql import ARRAY
 
+from odyssey import db
 from odyssey.utils.constants import DB_SERVER_TIME
 
-class Wearables(_Base):
+class Wearables(db.Model):
     """ Table that lists which supported wearables a client has. """
 
     __tablename__ = 'Wearables'
@@ -57,12 +43,13 @@ class Wearables(_Base):
              'User.user_id',
              ondelete="CASCADE"
         ),
-        nullable=False
+        nullable=False,
+        unique=True
     )
     """
     User ID number.
 
-    :type: int, foreign key to :attr:`User.user_id <odyssey.models.user.User.user_id>`
+    :type: int, unique, foreign key to :attr:`User.user_id <odyssey.models.user.User.user_id>`
     """
 
     has_oura = Column(Boolean, default=False, nullable=False)
@@ -115,7 +102,7 @@ class Wearables(_Base):
     """
 
 
-class WearablesOura(_Base):
+class WearablesOura(db.Model):
     """ Oura Ring specific information. """
 
     __tablename__ = 'WearablesOura'
@@ -133,12 +120,13 @@ class WearablesOura(_Base):
              'User.user_id',
              ondelete="CASCADE"
         ),
-        nullable=False
+        nullable=False,
+        unique=True
     )
     """
     Client ID number.
 
-    :type: int, foreign key to :attr:`User.user_id <odyssey.models.user.User.user_id>`
+    :type: int, unique, foreign key to :attr:`User.user_id <odyssey.models.user.User.user_id>`
     """
 
     created_at = db.Column(db.DateTime, default=DB_SERVER_TIME)
@@ -196,7 +184,7 @@ class WearablesOura(_Base):
     """
 
 
-class WearablesFitbit(_Base):
+class WearablesFitbit(db.Model):
     """ Fitbit specific information. """
 
     __tablename__ = 'WearablesFitbit'
@@ -214,12 +202,13 @@ class WearablesFitbit(_Base):
              'User.user_id',
              ondelete="CASCADE"
         ),
-        nullable=False
+        nullable=False,
+        unique=True
     )
     """
     Client ID number.
 
-    :type: int, foreign key to :attr:`User.user_id <odyssey.models.user.User.user_id>`
+    :type: int, unique, foreign key to :attr:`User.user_id <odyssey.models.user.User.user_id>`
     """
 
     created_at = db.Column(db.DateTime, server_default=DB_SERVER_TIME)
@@ -277,7 +266,7 @@ class WearablesFitbit(_Base):
     """
 
 
-class WearablesFreeStyle(_Base):
+class WearablesFreeStyle(db.Model):
     """ FreeStyle Libre continuous glucose monitoring wearable specific information. """
 
     __tablename__ = 'WearablesFreeStyle'
