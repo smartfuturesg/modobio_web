bf43efb319ce4d8004b9cdf48913044bf3b406d9
Zan Peeters <zan.peeters@atventurepartners.tech>
2021-05-06 19:09:06 -0700

    Update routes to work with improved tables and schemas.

--- src/odyssey/api/notifications/routes.py
+++ src/odyssey/api/notifications/routes.py
@@ -1,15 +1,16 @@
 from flask import request
 from flask_accepts import accepts, responds
 from flask_restx import Resource
+from sqlalchemy.exc import IntegrityError
 
 from odyssey import db
 from odyssey.api import api
 from odyssey.api.lookup.models import LookupNotifications
-from odyssey.api.notifications.models import Notifications, NotificationsPush
-from odyssey.api.notifications.schemas import NotificationSchema, PushNotificationSchema
+from odyssey.api.notifications.models import Notifications, NotificationsPushRegistration
+from odyssey.api.notifications.schemas import NotificationSchema, PushRegistrationSchema
 from odyssey.utils.auth import token_auth
 from odyssey.utils.constants import DB_SERVER_TIME
-from odyssey.utils.errors import InputError
+from odyssey.utils.errors import InputError, UnknownError
 
 ns = api.namespace('notifications', description='Endpoints for all types of notifications.')
 
@@ -21,56 +22,85 @@ class NotificationsEndpoint(Resource):
     @responds(schema=NotificationSchema(many=True), api=ns, status_code=200)
     def get(self, user_id):
         """ Returns a list of notifications for the given user_id. """
+        return Notifications.query.filter_by(user_id=user_id).all()
 
-        notifications = Notifications.query.filter_by(user_id=user_id).all()
+    @token_auth.login_required
+    @accepts(schema=NotificationSchema, api=ns)
+    @responds(status_code=201, api=ns)
+    def post(self, user_id):
+        """ Create a new notification for the user with user_id. """
+        request.parsed_obj.user_id = user_id
+        db.session.add(request.parsed_obj)
+        db.session.commit()
 
-        for notification in notifications:
-                notification.notification_type = LookupNotifications.query.filter_by(notification_type_id=notification.notification_type_id).one_or_none().notification_type
 
-        return notifications
-
-
-@ns.route('/<int:notification_id>/')
-@ns.doc(params={'notification_id': 'Notification ID number'})
+@ns.route('/<int:user_id>/<int:notification_id>/')
+@ns.doc(params={
+    'user_id': 'User ID number',
+    'notification_id': 'Notification ID number'})
 class NotificationsUpdateEndpoint(Resource):
 
     @token_auth.login_required
     @accepts(schema=NotificationSchema, api=ns)
-    @responds(schema=NotificationSchema, api=ns, status_code=201)
-    def put(self, notification_id):
+    @responds(status_code=200, api=ns)
+    def put(self, user_id, notification_id):
         """ Updates the notification specified by the given id number. """
 
-        notification = Notifications.query.filter_by(idx=notification_id).one_or_none()
+        notification = (
+            Notifications
+            .query
+            .filter_by(
+                user_id=user_id,
+                notification_id=notification_id)
+            .one_or_none())
 
         if not notification:
-            raise InputError(400, 'Invalid notification id.') 
+            raise InputError(400, 'Wrong notification_id or user_id.')
 
-        notification.update(request.json)
+        request.parsed_obj.user_id = user_id
+        request.parsed_obj.notification_id = notification_id
+        db.session.merge(request.parsed_obj)
         db.session.commit()
 
-        notification.notification_type = LookupNotifications.query.filter_by(notification_type_id=notification.notification_type_id).one_or_none().notification_type
 
-        return notification
-
-
-@ns.route('/push/<int:user_id>/')
+@ns.route('/push/register/<int:user_id>/')
 @ns.doc(params={'user_id': 'User ID number'})
-class PushNotificationsEndpoint(Resource):
+class PushRegistrationEndpoint(Resource):
 
-    @token_auth.login_required(user_type=('client',))
-    @responds(schema=PushNotificationSchema(many=True), api=ns, status_code=200)
+    @token_auth.login_required
+    @responds(schema=PushRegistrationSchema(many=True), api=ns, status_code=200)
     def get(self, user_id):
         """ Get all device IDs for user. """
-        pass
+        return NotificationsPushRegistration.query.filter_by(user_id=user_id).all()
 
-    @token_auth.login_required(user_type=('client',))
-    @accepts(schema=PushNotificationSchema, api=ns)
+    @token_auth.login_required
+    @accepts(schema=PushRegistrationSchema, api=ns)
+    @responds(status_code=201, api=ns)
     def post(self, user_id):
-        """ Register a new device ID (additive). """
-        pass
+        """ Register a new device ID (additive).
 
-    @token_auth.login_required(user_type=('client',))
-    @accepts(schema=PushNotificationSchema, api=ns)
+        New devices are added to the user, existing devices are not overwritten. Trying to
+        add an existing device is silently ignored.
+        """
+        request.parsed_obj.user_id = user_id
+        db.session.add(request.parsed_obj)
+        try:
+            db.session.commit()
+        except IntegrityError:
+            pass
+
+    @token_auth.login_required
+    @accepts(schema=PushRegistrationSchema, api=ns)
+    @responds(status_code=204, api=ns)
     def delete(self, user_id):
         """ Delete device ID (subtractive). """
-        pass
+        device = (
+            NotificationsPushRegistration
+            .query
+            .filter_by(
+                user_id=user_id,
+                device_id=request.parsed_obj.device_id)
+            .one_or_none())
+        if device:
+            db.session.delete(device)
+            db.session.commit()
