668df8a5d433e46256e2bb0a7487fb6b624e99ef
Zan Peeters <zan.peeters@atventurepartners.tech>
2021-05-21 11:46:31 -0700

    Revert "Change column from provider to channel."
    This reverts commit 1e70bafb3dfd0dbb5a369342c4578262dfc9bdf6.

--- src/odyssey/api/notifications/models.py
+++ src/odyssey/api/notifications/models.py
@@ -227,20 +227,18 @@ class NotificationsPushRegistration(db.Model):
     :type: str, max length 1024
     """
 
-    channel = db.Column(db.String(25))
+    provider = db.Column(db.String(25))
     """
-    Name of the channel of the push notification service. AWS calls
-    this the "platform application". Currently 'APNS' for Apple Push
-    Notification Service, 'APNS_VOIP' for Apple video call start, or
-    'FCM' for Firebase/Google Cloud Messaging.
+    Name of the provider of the push notification service. Currently
+    'APNS' for Apple Push Notification Service, 'APNS_VOIP' for Apple
+    video call start, or 'FCM' for Firebase/Google Cloud Messaging.
 
     :type: str, max length 25
     """
 
     arn = db.Column(db.String(256))
     """
-    The AWS Resource Name (ARN) of the notification channel (AWS SNS
-    platform application).
+    The AWS Resource Number (ARN) of the AWS SNS endpoint.
 
     :type: str, max length 256
     """
