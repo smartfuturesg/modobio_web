a278838e75bfe92a3b1b755edd8c8d90f09c9430
dalton.harenza <dalton.harenza@atlanticventurepartners.tech>
2021-06-11 14:18:19 -0700

    add base models, mixins, and base schema

---
+++ src/odyssey/utils/base/models.py
@@ -0,0 +1,72 @@
+from sqlalchemy.ext.declarative import declared_attr
+
+from odyssey import db
+from odyssey.utils.constants import DB_SERVER_TIME
+
+"""
+SQLAlchemy base model used to create all other models.
+Models that are declared with this base will automatically have "created_at" and "updated_at" columns.
+They will also have a __tablename__ that is identical to their class name.
+"""
+class BaseModel(db.Model):
+    __abstract__ = True
+
+    @declared_attr
+    def __tablename__(cls):
+        return cls.__name__
+
+    created_at = db.Column(db.DateTime, default=DB_SERVER_TIME)
+    """
+    Creation timestamp of this row in the database.
+
+    :type: :class:`datetime.datetime`
+    """
+
+    updated_at = db.Column(db.DateTime, default=DB_SERVER_TIME, onupdate=DB_SERVER_TIME)
+    """
+    Last update timestamp of this row in the database.
+
+    :type: :class:`datetime.datetime`
+    """
+
+"""
+Base model for tables that use the generic index name (idx)
+Inherits the attributes from the above base model but adds in an 'idx' column
+"""
+class BaseModelWithIdx(BaseModel):
+
+    __abstract__ = True
+
+    idx = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    """
+    Table index.
+
+    :type: int, primary key, autoincrement
+    """
+
+"""
+Mixin for tables that require a foriegn key to the User.user_id column
+BE CAREFUL to only use this mixin when you intend to user Cascade deletion with this fkey
+"""
+class UserIdFkeyMixin:
+    @declared_attr
+    def user_id(cls):
+        return db.Column(db.Integer, db.ForeignKey('User.user_id', ondelete="CASCADE"), nullable=False)
+        """
+        User ID number, foreign key to User.user_id
+
+        :type: int, foreign key to :attr:`User.user_id <odyssey.models.user.User.user_id>`
+        """
+
+"""
+Mixin for tables that require a reporter_id (user that reported the data) foreign key
+"""
+class ReporterIdFkeyMixin:
+    @declared_attr
+    def reporter_id(cls):
+        return db.Column(db.Integer, db.ForeignKey('User.user_id'), nullable=False)
+        """
+        Reporter ID number, foreign key to User.user_id
+
+        :type: int, foreign key to :attr:`User.user_id <odyssey.models.user.User.user_id>`
+        """
