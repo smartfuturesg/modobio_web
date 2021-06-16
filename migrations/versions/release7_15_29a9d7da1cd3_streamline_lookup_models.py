9f9399821ce28cfcc25d314d6dd8867b0ba5f1f4
dalton.harenza <dalton.harenza@atlanticventurepartners.tech>
2021-06-16 10:06:18 -0700

    align migrations

--- migrations/versions/release7_15_29a9d7da1cd3_streamline_lookup_models.py
+++ migrations/versions/release7_15_29a9d7da1cd3_streamline_lookup_models.py
@@ -1,7 +1,7 @@
 """streamline lookup models
 
 Revision ID: 29a9d7da1cd3
-Revises: 5fb1aae9de2f
+Revises: a7565530c9f5
 Create Date: 2021-06-14 12:31:40.729613
 
 """
@@ -11,7 +11,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = '29a9d7da1cd3'
-down_revision = '5fb1aae9de2f'
+down_revision = 'a7565530c9f5'
 branch_labels = None
 depends_on = None
 
