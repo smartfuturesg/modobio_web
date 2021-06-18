b65c0f199dd8c15579b8c0ef3e5cd91a0240147e
dalton.harenza <dalton.harenza@atlanticventurepartners.tech>
2021-06-18 15:25:53 -0700

    add org id and token to sql file

--- database/1024_add_organizations.sql
+++ database/1024_add_organizations.sql
@@ -1,7 +1,7 @@
 delete from "LookupOrganizations";
 
 
-INSERT INTO "LookupOrganizations" ("created_at", "updated_at", "org_name") 
+INSERT INTO "LookupOrganizations" ("created_at", "updated_at", "org_name", "token")
 VALUES
-(NOW(), NOW(), 'Wheel'),
-(NOW(), NOW(), 'VirtuDocs');
+(NOW(), NOW(), 'Wheel', 'WPCT8Z5D480', 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJvaWQiOiJXUENUOFo1RDQ4MCJ9.bT6AAZJGOAk7N3ON0hWKkfvq_WZIltzzsgcwvBoGx44'),
+(NOW(), NOW(), 'VirtuDocs', 'VD50W937ZM73', 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJvaWQiOiJWRDUwVzkzN1pNNzMifQ.is7xdFo41YNqdSsVZ-Vww6hKsYPLULiCVsCSY2kAgiM');
