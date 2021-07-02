03141a29a1d8cadb1f93670d56b623debcaacf09
ALEJANDRO <alejandro.lorenzo@atlanticventurepartners.tech>
2021-07-02 12:58:11 -0700

    reorganize seed user care team auths sql file

--- database/1027_seed_users_ehr_auths.sql
+++ database/1027_seed_users_ehr_auths.sql
@@ -1,20 +1,3 @@
-delete from "LookupEHRPages" 
-			where resource_group_id >= 1;
-alter sequence "LookupEHRPages_resource_group_id_seq"
-  restart with 1;
-
-INSERT INTO "LookupEHRPages" ("resource_group_id", "resource_group_name", "display_name", "access_group") 
-VALUES
-(1, 'identity_profile', 'Identity Profile' , 'general'),
-(2, 'health_profile', 'Health Profile' , 'general'),
-(3, 'activity_tracker', 'Activity Tracker Data' , 'general'),
-(4, 'medical_history', 'Medical History' , 'medical_doctor'),
-(5, 'medications', 'Medications' , 'medical_doctor'),
-(6, 'diagnostic_imaging', 'Diagnostic Imaging' , 'medical_doctor'),
-(7, 'blood_pressure', 'Blood Pressure' , 'medical_doctor'),
-(8, 'blood_chemistry', 'Blood Chemistry' , 'medical_doctor');
-
-
 --care team authorizations for seed users pro@modobio.com and name@modobio.com
 
 delete from "ClientClinicalCareTeam" 
@@ -48,4 +31,6 @@ insert into "ClientEHRPageAuthorizations" ("user_id", "team_member_user_id", "re
 (22, 14, 5, 'accepted'),
 (22, 14, 6, 'accepted'),
 (22, 14, 7, 'accepted'),
+(22, 14, 8, 'accepted');
+(22, 14, 8, 'accepted');
 (22, 14, 8, 'accepted');
