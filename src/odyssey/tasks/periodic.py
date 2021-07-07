ac6bb543edc5307aed69a8e92365fef5bc402b57
ALEJANDRO <alejandro.lorenzo@atlanticventurepartners.tech>
2021-07-07 12:10:56 -0700

    celery task for adding ehr permissions

--- src/odyssey/tasks/periodic.py
+++ src/odyssey/tasks/periodic.py
@@ -6,7 +6,7 @@ from sqlalchemy import delete, text
 from sqlalchemy import and_, or_, select
 
 from odyssey import celery, db
-from odyssey.tasks.tasks import upcoming_appointment_notification_2hr
+from odyssey.tasks.tasks import upcoming_appointment_care_team_permissions, upcoming_appointment_notification_2hr
 from odyssey.api.telehealth.models import TelehealthBookings
 from odyssey.api.client.models import ClientDataStorage
 from odyssey.api.lookup.models import LookupBookingTimeIncrements
@@ -29,7 +29,7 @@ def refresh_client_data_storage():
     return
 
 @celery.task()
-def deploy_upcoming_appointment_notifications(booking_window_id_start, booking_window_id_end, target_date=None):
+def deploy_upcoming_appointment_tasks(booking_window_id_start, booking_window_id_end, target_date=None):
     """
     Scan bookings for upcoming appointments. Deploy scheduled tasks which add an upcoming appointment
     notification to the UserNotification table within 2 hours of the booking. 
@@ -88,7 +88,7 @@ def deploy_upcoming_appointment_notifications(booking_window_id_start, booking_w
     if current_app.config['TESTING']:
         return upcoming_bookings
     
-    #schedule appointment notification tasks for 2 hrs before appointment
+    #schedule pre-appointment tasks
     for booking in upcoming_bookings:
         # create datetime object for scheduling tasks
         booking_start_time = db.session.execute(
@@ -96,9 +96,11 @@ def deploy_upcoming_appointment_notifications(booking_window_id_start, booking_w
                 where(LookupBookingTimeIncrements.idx == booking.booking_window_id_start_time_utc)
             ).scalars().one_or_none()
         notification_eta = datetime.combine(booking.target_date_utc, booking_start_time) - timedelta(hours = 2)
+        ehr_permissions_eta = datetime.combine(booking.target_date_utc, booking_start_time) - timedelta(minutes = 30)
 
         # Deploy scheduled task to update notifications table with upcoming booking notification
         upcoming_appointment_notification_2hr.apply_async((booking.idx,),eta=notification_eta)
+        upcoming_appointment_care_team_permissions.apply_async((booking.idx,),eta=ehr_permissions_eta)
 
     return 
 
@@ -113,25 +115,25 @@ celery.conf.beat_schedule = {
     # look for upcoming apppointment within moving windows:
     # (00:00 - 08:00)
     'check-for-upcoming-bookings-00-00': {
-        'task': 'odyssey.tasks.periodic.deploy_upcoming_appointment_notifications',
+        'task': 'odyssey.tasks.periodic.deploy_upcoming_appointment_tasks',
          'args': (1, 96),
         'schedule': crontab(hour=0, minute=0)
     },
     # (06:00 - 14:00)
     'check-for-upcoming-bookings-06-00': {
-        'task': 'odyssey.tasks.periodic.deploy_upcoming_appointment_notifications',
+        'task': 'odyssey.tasks.periodic.deploy_upcoming_appointment_tasks',
          'args': (73, 168),
         'schedule': crontab(hour=6, minute=0)
     },
     # (12:00 - 20:00)
     'check-for-upcoming-bookings-12-00': {
-        'task': 'odyssey.tasks.periodic.deploy_upcoming_appointment_notifications',
+        'task': 'odyssey.tasks.periodic.deploy_upcoming_appointment_tasks',
          'args': (145, 240),
         'schedule': crontab(hour=12, minute=0)
     },
     # (18:00 - 02:00, Next day)
     'check-for-upcoming-bookings-18-00': {
-        'task': 'odyssey.tasks.periodic.deploy_upcoming_appointment_notifications',
+        'task': 'odyssey.tasks.periodic.deploy_upcoming_appointment_tasks',
          'args': (217, 24),
         'schedule': crontab(hour=18, minute=0)
     },
--- src/odyssey/tasks/tasks.py
+++ src/odyssey/tasks/tasks.py
@@ -3,7 +3,9 @@ from datetime import datetime, timedelta
 from sqlalchemy import or_, select
 
 from odyssey import celery, db
-from odyssey.api.lookup.models import LookupBookingTimeIncrements
+from odyssey.api.client.models import ClientClinicalCareTeam, ClientEHRPageAuthorizations
+from odyssey.api.client.routes import ClinicalCareTeamMembers
+from odyssey.api.lookup.models import LookupBookingTimeIncrements, LookupEHRPages
 from odyssey.api.notifications.models import Notifications
 from odyssey.api.telehealth.models import TelehealthBookings
 from odyssey.api.user.models import User
@@ -83,3 +85,91 @@ def upcoming_appointment_notification_2hr(booking_id):
     db.session.commit()
 
     return
+
+@celery.task()
+def upcoming_appointment_care_team_permissions(booking_id):
+    """
+    Apply temporary care team access permissions to the staff member involved in the booking 
+
+    TODO:
+    30 mins before a call is scheduled via
+    """
+
+    # bring up resouce_group_ids required for medical doctor visits
+    # TODO: update this to align with other staff roles 
+    resource_ids_needed = db.session.execute(select(
+        LookupEHRPages.resource_group_id
+    ).where(LookupEHRPages.access_group.in_(['general','medical_doctor']))).scalars().all()
+    
+    # bring up booking
+    booking = db.session.execute(select(TelehealthBookings).where(TelehealthBookings.idx == booking_id)).scalars().one_or_none()
+    
+    if not booking:
+        return 
+    
+    # check if staff is already in care team and has permissions
+    care_team_staff = db.session.execute(select(ClientClinicalCareTeam
+    ).where(
+        ClientClinicalCareTeam.user_id==booking.client_user_id,
+        ClientClinicalCareTeam.team_member_user_id == booking.staff_user_id)
+    ).scalars().one_or_none()
+
+    # the staff member is already part of the user's care team
+    # ensure all necessary permissions are applied
+    if care_team_staff:
+        # bring up current permissions
+        current_resource_auths = db.session.execute(select(
+            ClientEHRPageAuthorizations
+        ).where(
+            ClientEHRPageAuthorizations.user_id == booking.client_user_id,
+            ClientEHRPageAuthorizations.team_member_user_id == booking.staff_user_id)
+        ).scalars().all()
+
+        current_resource_auth_ids = [item.resource_group_id for item in current_resource_auths if item.status == 'accepted']
+        current_resource_auth_ids_not_accepted = [item for item in current_resource_auths if item.status != 'accepted']
+
+        remaining_auths = len(set(resource_ids_needed) - set(current_resource_auth_ids))
+        
+        # if more resource authorizations are necessary, either add new auths or approve pending auths
+        if remaining_auths > 0:
+            # approve current pending auths 
+            if len(current_resource_auth_ids_not_accepted) > 0:
+                for pending_auth in current_resource_auth_ids_not_accepted:
+                    pending_auth.status = 'accepted'
+                    current_resource_auth_ids.append(pending_auth.resource_group_id)
+                
+                remaining_auths = len(set(resource_ids_needed) - set(current_resource_auth_ids))
+
+            # add new permissions
+            if remaining_auths > 0:
+                for resource_group_id in remaining_auths:
+                    db.session.add(ClientEHRPageAuthorizations(**{
+                        'user_id': booking.client_user_id,
+                        'team_member_user_id': booking.staff_user_id,
+                        'resource_group_id': resource_group_id
+                    }))
+        # all necessary authorizations already granted
+        else:
+            return
+
+
+    # staff not yet part of care team. Add them as a temporary member, assign permissions
+    else:
+        db.session.add(ClientClinicalCareTeam(**{
+            "team_member_user_id": booking.staff_user_id,
+            "user_id": booking.client_user_id,
+            "is_temporary": True}))
+
+        db.session.commit()
+
+        for resource_group_id in resource_ids_needed:
+            db.session.add(ClientEHRPageAuthorizations(**{
+                'user_id': booking.client_user_id,
+                'team_member_user_id': booking.staff_user_id,
+                'resource_group_id': resource_group_id
+            }))
+    
+    db.session.commit()
+
+    return
+
