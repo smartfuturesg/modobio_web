4f235655eb849cd09da766006d58b6be10f4dfbd
dalton.harenza <dalton.harenza@atlanticventurepartners.tech>
2021-07-09 13:56:42 -0700

    remove expiration from org tokens

--- database/1024_add_organizations.sql
+++ database/1024_add_organizations.sql
@@ -3,6 +3,6 @@ delete from "LookupOrganizations";
 
 INSERT INTO "LookupOrganizations" ("created_at", "updated_at", "org_name", "org_id", "org_token")
 VALUES
-(NOW(), NOW(), 'Wheel', 'WPCT8Z5D480', 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE2NDE2MTQ1OTEsInR0eXBlIjoib3JnYW5pemF0aW9uIiwib25hbWUiOiJXaGVlbCJ9.f-29nVBd1Qty-IV2BBi3sc_m82glvXWhhQL4HzYWyzs'),
-(NOW(), NOW(), 'VirtuDocs', 'VD50W937ZM73', 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE2NDE2MTQ1NjIsInR0eXBlIjoib3JnYW5pemF0aW9uIiwib25hbWUiOiJWaXJ0dURvY3MifQ.aBKO0Z3ZQeWpdPWVsTlkf0OBhXHkFZ0IIMQE5T4P-Dk'),
-(NOW(), NOW(), 'InstaMed', 'IMWYZ35L3D71', 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE2NDE2MTQ1MzcsInR0eXBlIjoib3JnYW5pemF0aW9uIiwib25hbWUiOiJJbnN0YU1lZCJ9.8W2q_UpiBdqifihCv3b2HiW2NL6QkpdHHekTkLTnrDE');
+(NOW(), NOW(), 'Wheel', 'WPCT8Z5D480', 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0dHlwZSI6Im9yZ2FuaXphdGlvbiIsIm9uYW1lIjoiV2hlZWwifQ.VR2OR3yi9AgFowt2_txcvCLZu62PEwvo3gUyqwfWwk0'),
+(NOW(), NOW(), 'VirtuDocs', 'VD50W937ZM73', 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0dHlwZSI6Im9yZ2FuaXphdGlvbiIsIm9uYW1lIjoiVmlydHVEb2NzIn0.3Nc_9X5CFHeaf4d9_Uzt3mR3pSNm1KNgemxkXE9VeqU'),
+(NOW(), NOW(), 'InstaMed', 'IMWYZ35L3D71', 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0dHlwZSI6Im9yZ2FuaXphdGlvbiIsIm9uYW1lIjoiSW5zdGFNZWQifQ.BGQgeZ_vAO40S1rjDusPCu1DAGryyBulm1i72E9ze4Q');
