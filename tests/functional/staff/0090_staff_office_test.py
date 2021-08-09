fa54d9b064737bc118d69eb8150a6d6b02b0feba
dalton.harenza <dalton.harenza@atlanticventurepartners.tech>
2021-08-09 10:08:02 -0700

    update test format

--- tests/functional/staff/0090_staff_office_test.py
+++ tests/functional/staff/0090_staff_office_test.py
@@ -2,16 +2,10 @@ from flask import current_app
 from .data import staff_office_data
 from flask.json import dumps
 
-def test_post_staff_office(test_client, init_database, staff_auth_header):
-    """
-    GIVEN an api end point for storing data about a professional's office
-    WHEN the '/staff/office/' resource is requested (POST)
-    THEN check that the office data is stored
-    """
-
+def test_post_staff_office(test_client):
     #test with invalid country id, should return 404
-    response = test_client.post('/staff/offices/2/',
-                                headers=staff_auth_header,
+    response = test_client.post(f'/staff/offices/{test_client.staff_id}/',
+                                headers=test_client.staff_auth_header,
                                 data=dumps(staff_office_data['invalid_country_id']),
                                 content_type='application/json')
     
@@ -19,8 +13,8 @@ def test_post_staff_office(test_client, init_database, staff_auth_header):
     assert response.status_code == 404
 
     #test with a field that exceeds its maximum length, should return 400
-    response = test_client.post('/staff/offices/2/',
-                                headers=staff_auth_header,
+    response = test_client.post(f'/staff/offices/{test_client.staff_id}/',
+                                headers=test_client.staff_auth_header,
                                 data=dumps(staff_office_data['too_long']),
                                 content_type='application/json')
 
@@ -28,8 +22,8 @@ def test_post_staff_office(test_client, init_database, staff_auth_header):
     assert response.status_code == 400
 
     #test with invalid country id, should return 404
-    response = test_client.post('/staff/offices/2/',
-                                headers=staff_auth_header,
+    response = test_client.post(f'/staff/offices/{test_client.staff_id}/',
+                                headers=test_client.staff_auth_header,
                                 data=dumps(staff_office_data['invalid_phone_type']),
                                 content_type='application/json')
     
@@ -37,8 +31,8 @@ def test_post_staff_office(test_client, init_database, staff_auth_header):
     assert response.status_code == 400
 
     #test with valid data, should return 201
-    response = test_client.post('/staff/offices/2/',
-                                headers=staff_auth_header,
+    response = test_client.post(f'/staff/offices/{test_client.staff_id}/',
+                                headers=test_client.staff_auth_header,
                                 data=dumps(staff_office_data['normal_data']),
                                 content_type='application/json')
     
@@ -48,31 +42,19 @@ def test_post_staff_office(test_client, init_database, staff_auth_header):
     assert response.json['city'] == 'Phoenix'
     assert response.json['phone'] == '1234567'
 
-def test_get_staff_office(test_client, init_database, staff_auth_header):
-    """
-    GIVEN an api end point for viewing a staff's office
-    WHEN the '/staff/office/' resource is requested (GET)
-    THEN check that the response is valid
-    """
-
-    response = test_client.get('/staff/offices/2/',
-                                headers=staff_auth_header)
+def test_get_staff_office(test_client):
+    response = test_client.get(f'/staff/offices/{test_client.staff_id}/',
+                                headers=test_client.staff_auth_header)
     
     # some simple checks for validity
     assert response.status_code == 200
     assert response.json['country'] == 'USA'
     assert response.json['phone'] == '1234567'
 
-def test_put_staff_profile(test_client, init_database, staff_auth_header):
-    """
-    GIVEN an api end point for updating data about a professional's office
-    WHEN the '/staff/office/' resource is requested (PUT)
-    THEN check that the office data is updated
-    """
-
+def test_put_staff_profile(test_client):
     #test with invalid country id, should return 404
-    response = test_client.put('/staff/offices/2/',
-                                headers=staff_auth_header,
+    response = test_client.put(f'/staff/offices/{test_client.staff_id}/',
+                                headers=test_client.staff_auth_header,
                                 data=dumps(staff_office_data['invalid_country_id']),
                                 content_type='application/json')
     
@@ -80,8 +62,8 @@ def test_put_staff_profile(test_client, init_database, staff_auth_header):
     assert response.status_code == 404
 
     #test with a field that exceeds its maximum length, should return 400
-    response = test_client.put('/staff/offices/2/',
-                                headers=staff_auth_header,
+    response = test_client.put(f'/staff/offices/{test_client.staff_id}/',
+                                headers=test_client.staff_auth_header,
                                 data=dumps(staff_office_data['too_long']),
                                 content_type='application/json')
     
@@ -89,8 +71,8 @@ def test_put_staff_profile(test_client, init_database, staff_auth_header):
     assert response.status_code == 400
 
     #test with invalid country id, should return 404
-    response = test_client.put('/staff/offices/2/',
-                                headers=staff_auth_header,
+    response = test_client.put(f'/staff/offices/{test_client.staff_id}/',
+                                headers=test_client.staff_auth_header,
                                 data=dumps(staff_office_data['invalid_phone_type']),
                                 content_type='application/json')
     
@@ -98,8 +80,8 @@ def test_put_staff_profile(test_client, init_database, staff_auth_header):
     assert response.status_code == 400
 
     #test with valid data, should return 201
-    response = test_client.put('/staff/offices/2/',
-                                headers=staff_auth_header,
+    response = test_client.put(f'/staff/offices/{test_client.staff_id}/',
+                                headers=test_client.staff_auth_header,
                                 data=dumps(staff_office_data['normal_data_2']),
                                 content_type='application/json')
     
