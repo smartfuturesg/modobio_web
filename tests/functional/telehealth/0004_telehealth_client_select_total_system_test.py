f0df0dfab729c4cb11bd254e9879e321276ab2d9
dalton.harenza <dalton.harenza@atlanticventurepartners.tech>
2021-08-31 14:29:39 -0700

    use first instead of one_or_none

--- tests/functional/telehealth/0004_telehealth_client_select_total_system_test.py
+++ tests/functional/telehealth/0004_telehealth_client_select_total_system_test.py
@@ -316,7 +316,7 @@ def test_bookings_meeting_room_access(test_client):
         token = response.json.get('token')
         auth_header = {'Authorization': f'Bearer {token}'}
 
-        room_id = TelehealthBookings.query.filter_by(client_user_id=test_client.client_id, staff_user_id=test_client.staff_id).one_or_none().idx
+        room_id = TelehealthBookings.query.filter_by(client_user_id=test_client.client_id, staff_user_id=test_client.staff_id).first().idx
         response = test_client.get(
         f'/telehealth/bookings/meeting-room/access-token/{room_id}/', headers=test_client.staff_auth_header)
         print(response.data)
