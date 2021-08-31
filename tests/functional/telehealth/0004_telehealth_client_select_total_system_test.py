8840349552ce50a4776b952d08b4348fe5b82340
dalton.harenza <dalton.harenza@atlanticventurepartners.tech>
2021-08-31 12:48:35 -0700

    debug test

--- tests/functional/telehealth/0004_telehealth_client_select_total_system_test.py
+++ tests/functional/telehealth/0004_telehealth_client_select_total_system_test.py
@@ -319,6 +319,7 @@ def test_bookings_meeting_room_access(test_client):
         response = test_client.get(
         f'/telehealth/bookings/meeting-room/access-token/{test_client.client_id}/', headers=auth_header)
         
+        print(response.data)
         assert response.status_code == 200
 
         """Below will test the following payment features that required a booking to be accessed
