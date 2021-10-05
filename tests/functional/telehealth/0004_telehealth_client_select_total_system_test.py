b4358c887c93bfe8fb60e4ac40ce46776b1b3989
Steve.Thach <steve.thach@atlanticventurepartners.tech>
2021-10-05 16:56:20 -0700

    testing session cost 60, updated refund amount

--- tests/functional/telehealth/0004_telehealth_client_select_total_system_test.py
+++ tests/functional/telehealth/0004_telehealth_client_select_total_system_test.py
@@ -359,7 +359,7 @@ def test_bookings_meeting_room_access(test_client):
         data=dumps(payment_refund_data),
         content_type='application.json'
     )
-    breakpoint()
+
     assert response.status_code == 201
 
     response = test_client.get(
