856b09903c30763fbdd2f93968c73d890f12043e
dalton.harenza <dalton.harenza@atlanticventurepartners.tech>
2021-10-06 14:55:55 -0700

    make roles changes in new script

--- database/0001_add_roles.sql
+++ database/0001_add_roles.sql
@@ -3,20 +3,20 @@ delete from "LookupRoles"
 alter sequence "LookupRoles_idx_seq"
   restart with 1;
 
-INSERT INTO "LookupRoles" ("created_at", "updated_at","role_name","display_name","alt_role_name","icon","is_practitioner","has_client_data_access","active") 
+INSERT INTO "LookupRoles" ("created_at", "updated_at","role_name","display_name","alt_role_name","is_practitioner","color","icon","has_client_data_access","active") 
 
 VALUES
-('NOW()','NOW()','beautician','Beautician','esthetician','Beautician.svg',True,True,False),
-('NOW()','NOW()','chef','Chef',NULL,'ChefP.svg',True,True,True),
-('NOW()','NOW()','client_services','Client Services',NULL,'CommunityM.svg',False,True,True),
-('NOW()','NOW()','community_manager','Community Manager','ambassador',NULL,False,False,True),
-('NOW()','NOW()','data_scientist','Data Scientist',NULL,NULL,False,False,False),
-('NOW()','NOW()','dietitian','Dietitian',NULL,'DietitianP.svg',True,True,True),
-('NOW()','NOW()','medical_doctor','Medical Doctor','physician, clinician','MedicalDoctorP.svg',True,True,True),
-('NOW()','NOW()','nurse','Nurse',NULL,'NurseP.svg',True,True,False),
-('NOW()','NOW()','nutritionist','Nutritionist',NULL,NULL,True,True,False),
-('NOW()','NOW()','physical_therapist','Physical Therapist','physiotherapist','PhysicalTherapistP.svg',True,True,False),
-('NOW()','NOW()','staff_admin','Staff Admin','practitioner_admin','SystemAdmin.svg',False,False,True),
-('NOW()','NOW()','system_admin','System Admin',NULL,NULL,False,False,True),
-('NOW()','NOW()','therapist','Therapist','mental_health_specialist','Therapist.svg',True,True,True),
-('NOW()','NOW()','trainer','Trainer','physical_trainer, personal_trainer','TrainerP.svg',True,True,True);
+('NOW()','NOW()','staff_admin','Staff Admin','Practioner Admin',False,'#000000',NULL,False,False),
+('NOW()','NOW()','data_scientist','Data Scientist',NULL,False,'#000000',NULL,False,False),
+('NOW()','NOW()','community_manager','Community Manager','ambassador',False,'#000000',NULL,False,False),
+('NOW()','NOW()','physical_therapist','Physical Therapist','physiotherapist',True,'#fec619','physiotherapist.svg',True,False),
+('NOW()','NOW()','dietitian','Dietitian',NULL,True,'#000000',NULL,True,False),
+('NOW()','NOW()','nutritionist','Nutritionist',NULL,True,'#4661af','nutritionist - 1.svg',True,False),
+('NOW()','NOW()','therapist','Therapist','psychologist',True,'#000000',NULL,True,False),
+('NOW()','NOW()','nurse_practitioner','Nurse Practitioner',NULL,True,'#000000',NULL,True,False),
+('NOW()','NOW()','nurse','Nurse',NULL,True,'#000000',NULL,True,False),
+('NOW()','NOW()','physician_assistant','Physicians Assistant',NULL,True,'#000000',NULL,True,False),
+('NOW()','NOW()','system_admin','System Admin',NULL,False,'#000000',NULL,False,True),
+('NOW()','NOW()','client_services','Client Services',NULL,False,'#000000',NULL,True,True),
+('NOW()','NOW()','medical_doctor','Medical Doctor','physician',True,'#d71e3e','doctor.svg',True,True),
+('NOW()','NOW()','trainer',	'Trainer','physical_trainer, personal_trainer',True,'#aef2ea','physical-training.svg',True,True);
---
+++ database/1031_update_roles.sql
@@ -0,0 +1,16 @@
+--remove roles that no longer exist
+DELETE FROM "LookupRoles" WHERE 'role_name' = 'nurse_practitioner';
+DELETE FROM "LookupRoles" WHERE 'role_name' = 'physician_assistant';
+
+--remove notion of color
+ALTER TABLE "LookupRoles" DROP COLUMN 'color';
+
+--insert new roles
+INSERT INTO "LookupRoles" ("created_at", "updated_at","role_name","display_name","alt_role_name","icon","is_practitioner","has_client_data_access","active") 
+VALUES
+('NOW()','NOW()','beautician','Beautician','esthetician','Beautician.svg',True,True,False),
+('NOW()','NOW()','chef','Chef',NULL,'ChefP.svg',True,True,True);
+
+--update roles active flags
+UPDATE "LookupRoles" SET 'active' = True WHERE "role_name" IN ('client_services','community_manager','dietitian','medical_doctor','staff_admin','system_admin','therapist','trainer');
+UPDATE "LookupRoles" SET 'active' = False WHERE "role_name" IN ('data_scientist', 'nurse', 'nutritionist', 'physical_therapist');
