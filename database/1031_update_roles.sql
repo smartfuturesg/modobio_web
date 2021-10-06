6b812a9faba70eeffc119075cc1441afb24e7dac
dalton.harenza <dalton.harenza@atlanticventurepartners.tech>
2021-10-06 15:12:48 -0700

    fix syntax errors

--- database/1031_update_roles.sql
+++ database/1031_update_roles.sql
@@ -1,9 +1,9 @@
 --remove roles that no longer exist
-DELETE FROM "LookupRoles" WHERE 'role_name' = 'nurse_practitioner';
-DELETE FROM "LookupRoles" WHERE 'role_name' = 'physician_assistant';
+DELETE FROM "LookupRoles" WHERE "role_name" = 'nurse_practitioner';
+DELETE FROM "LookupRoles" WHERE "role_name" = 'physician_assistant';
 
 --remove notion of color
-UPDATE "LookupRoles" SET 'color' = NULL;
+UPDATE "LookupRoles" SET "color" = NULL;
 
 --insert new roles
 INSERT INTO "LookupRoles" ("created_at", "updated_at","role_name","display_name","alt_role_name","icon","is_practitioner","has_client_data_access","active") 
@@ -12,5 +12,5 @@ VALUES
 ('NOW()','NOW()','chef','Chef',NULL,'ChefP.svg',True,True,True);
 
 --update roles active flags
-UPDATE "LookupRoles" SET 'active' = True WHERE "role_name" IN ('client_services','community_manager','dietitian','medical_doctor','staff_admin','system_admin','therapist','trainer');
-UPDATE "LookupRoles" SET 'active' = False WHERE "role_name" IN ('data_scientist', 'nurse', 'nutritionist', 'physical_therapist');
+UPDATE "LookupRoles" SET "active" = True WHERE "role_name" IN ('client_services','community_manager','dietitian','medical_doctor','staff_admin','system_admin','therapist','trainer');
+UPDATE "LookupRoles" SET "active" = False WHERE "role_name" IN ('data_scientist', 'nurse', 'nutritionist', 'physical_therapist');
