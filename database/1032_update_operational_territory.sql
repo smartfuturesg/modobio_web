df050c2023cbc82233a699f978a9296500d769ab
dalton.harenza <dalton.harenza@atlanticventurepartners.tech>
2021-10-07 11:32:06 -0700

    remove unused operational_territories

---
+++ database/1032_update_operational_territory.sql
@@ -0,0 +1,8 @@
+--remove territories from StaffOperationalTerritoriesFirst to avoid foreign key errors
+DELETE FROM "StaffOperationalTerritories" WHERE "operational_territory_id" IN (1, 3, 4);
+
+--delete all territories except for Florida, then change Florida's idx to 1 and restart sequence
+DELETE FROM "LookupTerritoriesOfOperations" WHERE "idx" IN (1, 3, 4);
+UPDATE "LookupTerritoriesOfOperations" SET "idx" = 1 WHERE "idx" = 2;
+ALTER SEQUENCE "LookupTerritoriesOfOperations_idx_seq"
+  restart with 2;
