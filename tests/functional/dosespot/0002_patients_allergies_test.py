b0091cefd82d055c01e4232716093179ad11d40f
Steve.Thach <steve.thach@atlanticventurepartners.tech>
2021-10-22 16:37:22 -0700

    added pytest import

--- tests/functional/dosespot/0002_patients_allergies_test.py
+++ tests/functional/dosespot/0002_patients_allergies_test.py
@@ -1,3 +1,5 @@
+import pytest
+
 @pytest.mark.skip('Dosespot API down')
 def test_get_patient_ds_allergies_as_client(test_client):
     response = test_client.get(f'/dosespot/allergies/{test_client.client_id}/',
