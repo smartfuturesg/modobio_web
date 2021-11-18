dbb74ddfa4ac8fbec10e7b733e1d5ce7b539e6ba
dalton.harenza <dalton.harenza@atlanticventurepartners.tech>
2021-11-18 12:46:56 -0700

    add cardholder name to payment methods

--- tests/functional/payment/data.py
+++ tests/functional/payment/data.py
@@ -11,26 +11,31 @@ payment_methods_data = {
     'normal_data': {
         'token': '4111111111111111',
         'expiration': '04/25',
+        'cardholder_name': "Test Tester",
         'is_default': True
     },
     'normal_data_2': {
         'token': '5500000000000004',
         'expiration': '04/25',
+        'cardholder_name': "Test Tester",
         'is_default': True
     },
     'normal_data_3': {
         'token': '6011000000000004',
         'expiration': '04/25',
+        'cardholder_name': "Test Tester",
         'is_default': False
     },
     'invalid_card': {
         'token': '9999999999999999',
         'expiration': '04/25',
+        'cardholder_name': "Test Tester",
         'is_default': True
     },
     'expired': {
         'token': '4111111111111111',
         'expiration': '01/20',
+        'cardholder_name': "Test Tester",
         'is_default': False
     }
 }
