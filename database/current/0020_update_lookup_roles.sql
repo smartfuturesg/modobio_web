8c0067be941cba83af3d5c09d9568cc5d69d1132
dalton.harenza <dalton.harenza@atlanticventurepartners.tech>
2022-01-11 17:00:11 -0700

    update roles lookup table

---
+++ database/current/0020_update_lookup_roles.sql
@@ -0,0 +1,5 @@
+UPDATE "LookupRoles" SET "icon" = 'Beautician.svg' WHERE "role_name" = 'beautician';
+UPDATE "LookupRoles" SET "icon" = 'ClientsServicesP.svg' WHERE "role_name" = 'client_services';
+UPDATE "LookupRoles" SET "icon" = 'CommunityM.svg' WHERE "role_name" = 'community_manager';
+UPDATE "LookupRoles" SET "icon" = 'StaffP.svg' WHERE "role_name" = 'staff_admin';
+UPDATE "LookupRoles" SET "icon" = 'SystemAdmin.svg' WHERE "role_name" = 'system_admin';
