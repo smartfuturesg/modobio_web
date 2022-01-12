2f179c20894047bd44b3e205ed9144f00fc12216
dalton.harenza <dalton.harenza@atlanticventurepartners.tech>
2022-01-11 17:22:22 -0700

    fix test ranges test url

--- tests/functional/lookup/2027_lookup_blood_tests_test.py
+++ tests/functional/lookup/2027_lookup_blood_tests_test.py
@@ -7,7 +7,7 @@ def test_get_blood_tests(test_client):
     assert response.status_code == 200
 
     response = test_client.get(
-        '/lookup/bloodtests/ranges/?modobio_test_code=CMP001',
+        '/lookup/bloodtests/ranges/CMP001/',
         headers=test_client.client_auth_header,
         content_type='application/json')
     
