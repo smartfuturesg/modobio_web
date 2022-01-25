ed11525a8c070582055a61242cd9f24b27c91ee9
dalton.harenza <dalton.harenza@atlanticventurepartners.tech>
2022-01-25 14:48:50 -0700

    align migrations

--- migrations/versions/release-1.1_01_9522e43e5d49_email_verification.py
+++ migrations/versions/release-1.1_01_9522e43e5d49_email_verification.py
@@ -1,7 +1,7 @@
 """Adds email field to UserPendingEmailVerifications
 
 Revision ID: 9522e43e5d49
-Revises: fb14f8b40f12
+Revises: 28f6800e6909
 Create Date: 2022-01-10 12:14:44.076869
 
 """
@@ -11,7 +11,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = '9522e43e5d49'
-down_revision = 'f9ed26de0007'
+down_revision = '28f6800e6909'
 branch_labels = None
 depends_on = None
 
