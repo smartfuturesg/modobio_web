7c76d9772a1b8ae0c704939b70d49ad6c2d6960c
Elizabeth.Graham <elizabeth.graham@atventurepartners.tech>
2022-02-25 13:39:08 -0600

    Refactor maintenance scheduling code

--- src/odyssey/api/maintenance/schemas.py
+++ src/odyssey/api/maintenance/schemas.py
@@ -1,10 +1,11 @@
+from xmlrpc.client import _iso8601_format
 from marshmallow import (
     Schema,
     fields
 )
 from datetime import datetime
 from flask import current_app
-import pytz
+from pytz import timezone
 import logging
 
 logger = logging.getLogger(__name__)
@@ -12,10 +13,10 @@ logger = logging.getLogger(__name__)
 
 class MaintenanceBlocksCreateSchema(Schema):
     # Must be DateTime objects in the future
-    start_time = fields.DateTime(format="iso", required=True, validate=lambda x: x > datetime.now(
-        tz=pytz.timezone(current_app.config['MAINTENANCE_TIMEZONE'])))
-    end_time = fields.DateTime(format="iso", required=True, validate=lambda x: x > datetime.now(
-        tz=pytz.timezone(current_app.config['MAINTENANCE_TIMEZONE'])))
+    start_time = fields.DateTime(format="iso", required=True, validate=lambda x: x.replace(tzinfo=timezone(current_app.config['MAINTENANCE_TIMEZONE'])) > datetime.now(
+        tz=timezone(current_app.config['MAINTENANCE_TIMEZONE'])))
+    end_time = fields.DateTime(format="iso", required=True, validate=lambda x: x.replace(tzinfo=timezone(current_app.config['MAINTENANCE_TIMEZONE'])) > datetime.now(
+        tz=timezone(current_app.config['MAINTENANCE_TIMEZONE'])))
     comments = fields.String()
 
 
