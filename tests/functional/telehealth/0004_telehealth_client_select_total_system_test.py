a48bbd77681a6651e8c05fca0de3252bfd730f4d
Alejandro <alejandro.lorenzo@atventurepartners.tech>
2022-04-11 09:59:15 -0700

    update client/time-select test to use staff_availabilities fixture

--- tests/functional/telehealth/0004_telehealth_client_select_total_system_test.py
+++ tests/functional/telehealth/0004_telehealth_client_select_total_system_test.py
@@ -183,12 +183,13 @@ def test_generate_client_queue(test_client, payment_method):
 
     assert response.status_code == 201
 
-def test_client_time_select(test_client):
+def test_client_time_select(test_client, staff_availabilities):
     response = test_client.get(
         f'/telehealth/client/time-select/{test_client.client_id}/',
         headers=test_client.client_auth_header)
+        
     assert response.status_code == 200
-    assert response.json['total_options'] == 30
+    assert response.json['total_options'] == 95
 
 def test_full_system_with_settings(test_client, payment_method, telehealth_staff):
     """
