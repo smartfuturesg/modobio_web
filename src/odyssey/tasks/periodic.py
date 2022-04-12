bcda1f3cd621022618411e687e4d4c5e124e92c4
dalton.harenza <dalton.harenza@atlanticventurepartners.tech>
2022-04-12 14:51:18 -0700

    rework upcoming bookings task

--- src/odyssey/tasks/periodic.py
+++ src/odyssey/tasks/periodic.py
@@ -59,67 +59,32 @@ def refresh_client_data_storage():
     return
 
 @celery.task()
-def deploy_upcoming_appointment_tasks(booking_window_id_start, booking_window_id_end, target_date=None):
+def deploy_upcoming_appointment_tasks():
     """
-    Scan bookings for upcoming appointments. Deploy scheduled tasks which add an upcoming appointment
-    notification to the UserNotification table within 2 hours of the booking. 
-
-    00:00 - 08:00
-    06:00 - 14:00
-    12:00 - 20:00
-    18:00 - 02:00
-    00:00 - 08:00 
-
-    Parameters
-    ----------
-    target_date : to be used if testing. Otherwise date is set using system time (UTC)
-    booking_window_id_start: start of booking window provided in UTC
-    booking_window_id_end: end of booking window provided in UTC
+    This task will scan the TelehealthBookings table for appointments that are not yet notified and
+    are less than 2 hours away. It will then create a notification for the client and practitioner 
+    involved with the booking and deploy the needed ehr permissions to the practitioner.
     """
-    # grab the current date for the queries below
-    if not target_date:
-        target_date = date.today()
+    #get all bookings that are sheduled <24 hours away and have not been charged yet
+    logger.info('deploying upcoming appointments task')
+    target_time = datetime.now(timezone.utc) + timedelta(hours=2)
+    target_time_window = get_time_index(target_time)
+    logger.info(f'charge bookings task time window: {target_time_window}')
     
-    # compensate for scanning window inbetween days
-    if booking_window_id_start > booking_window_id_end:
-        # bookings beginning on current day
-        bookings_1 = db.session.execute(
-            select(TelehealthBookings). 
-                where(and_(
-                TelehealthBookings.target_date_utc==target_date, 
-                TelehealthBookings.booking_window_id_start_time_utc >= booking_window_id_start , # ex. 18:00
-                or_( #ex  meeting ends between 18:00 and 02:00
-                    TelehealthBookings.booking_window_id_end_time_utc <= booking_window_id_end, #02:00
-                    TelehealthBookings.booking_window_id_end_time_utc > booking_window_id_start),
-                )) 
-            ).scalars().all()
-
-        # bookings begining on following day of scanning window
-        bookings_2 = db.session.execute(
-            select(TelehealthBookings). 
-            where(and_(
-                TelehealthBookings.target_date_utc==target_date+timedelta(days=1), 
-                TelehealthBookings.booking_window_id_start_time_utc >= 0 , #00:00
-                TelehealthBookings.booking_window_id_end_time_utc <= booking_window_id_end, #02:00
-                )) 
-            ).scalars().all()
-        upcoming_bookings = bookings_1 + bookings_2
-    else:
-        upcoming_bookings = db.session.execute(
-            select(TelehealthBookings). 
-            where(and_(
-                TelehealthBookings.target_date_utc==target_date, 
-                TelehealthBookings.booking_window_id_start_time_utc >= booking_window_id_start , 
-                TelehealthBookings.booking_window_id_end_time_utc <= booking_window_id_end, 
-                )) 
-            ).scalars().all()
+    bookings = TelehealthBookings.query.filter(TelehealthBookings.status == 'Accepted', TelehealthBookings.notified == True) \
+        .filter(or_(
+            and_(TelehealthBookings.booking_window_id_start_time_utc >= target_time_window, TelehealthBookings.target_date_utc == datetime.now(timezone.utc).date()),
+            and_(TelehealthBookings.booking_window_id_start_time_utc <= target_time_window, TelehealthBookings.target_date_utc == target_time.date())
+        )).all()
 
     # do not deploy appointment notifications in testing
     if config.TESTING:
-        return upcoming_bookings
+        return bookings
     
     #schedule pre-appointment tasks
-    for booking in upcoming_bookings:
+    for booking in bookings:
+        logger.info(f'deploying notifcation and care team tasks for booking with id {booking.idx}')
+        booking.notified = True
         # create datetime object for scheduling tasks
         booking_start_time = db.session.execute(
                 select(LookupBookingTimeIncrements.start_time).
@@ -131,8 +96,8 @@ def deploy_upcoming_appointment_tasks(booking_window_id_start, booking_window_id
         # Deploy scheduled task to update notifications table with upcoming booking notification
         upcoming_appointment_notification_2hr.apply_async((booking.idx,),eta=notification_eta)
         upcoming_appointment_care_team_permissions.apply_async((booking.idx,),eta=ehr_permissions_eta)
-
-    return 
+    db.session.commit()
+    logger.info('upcoming bookings task completed')
 
 @celery.task()
 def deploy_appointment_transcript_store_tasks(target_date=None):
@@ -408,30 +373,10 @@ def close_previous_db_connection(**kwargs):
         db.engine.dispose()
 
 celery.conf.beat_schedule = {
-    # look for upcoming apppointment within moving windows:
-    # (00:00 - 08:00)
-    'check-for-upcoming-bookings-00-00': {
+    # look for upcoming apppointments:
+    'check-for-upcoming-bookings': {
         'task': 'odyssey.tasks.periodic.deploy_upcoming_appointment_tasks',
-         'args': (1, 96),
-        'schedule': crontab(hour=0, minute=0)
-    },
-    # (06:00 - 14:00)
-    'check-for-upcoming-bookings-06-00': {
-        'task': 'odyssey.tasks.periodic.deploy_upcoming_appointment_tasks',
-         'args': (73, 168),
-        'schedule': crontab(hour=6, minute=0)
-    },
-    # (12:00 - 20:00)
-    'check-for-upcoming-bookings-12-00': {
-        'task': 'odyssey.tasks.periodic.deploy_upcoming_appointment_tasks',
-         'args': (145, 240),
-        'schedule': crontab(hour=12, minute=0)
-    },
-    # (18:00 - 02:00, Next day)
-    'check-for-upcoming-bookings-18-00': {
-        'task': 'odyssey.tasks.periodic.deploy_upcoming_appointment_tasks',
-         'args': (217, 24),
-        'schedule': crontab(hour=18, minute=0)
+        'schedule': crontab(minute='*/10')
     },
     #temporary member cleanup
     'cleanup_temporary_care_team': {
