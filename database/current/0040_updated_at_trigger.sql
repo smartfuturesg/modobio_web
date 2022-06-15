2935907613d4fe63aac25ba0c61a87e3166c4831
Samson Bankole <samson.bankole@atventurepartners.tech>
2022-06-15 15:52:54 -0500

    Creates event trigger and function to add trigger to newly created tables

--- database/current/0040_updated_at_trigger.sql
+++ database/current/0040_updated_at_trigger.sql
@@ -11,9 +11,10 @@ END;
 $$;
 
 ----------------------------------------------------------
--- Sets refresh_updated_at trigger to all tables in the
--- database that has the updated_at column 
+-- Sets refresh_updated_at trigger to all existing tables 
+-- in the database that has the updated_at column 
 ----------------------------------------------------------
+
 DO $$
 DECLARE
     t record;
@@ -29,3 +30,40 @@ BEGIN
     END LOOP;
 END;
 $$ LANGUAGE plpgsql;
+
+----------------------------------------------------------
+-- event trigger function that loops over any tables that 
+-- doesn't have an updated_at_trigger and creates the 
+-- update_at_trigger for it
+----------------------------------------------------------
+CREATE function create_trigger_after_create_event() 
+RETURNS event_trigger  AS $event$
+BEGIN
+    do $$
+    DECLARE
+        r record;
+	    BEGIN
+	        FOR r IN 
+	            SELECT * from information_schema."columns" c
+                WHERE c.column_name = 'updated_at' AND NOT EXISTS
+                    (SELECT * from information_schema.triggers t 
+                    WHERE c.table_name = t.event_object_table AND t.trigger_name = 'updated_at_trigger')
+	        LOOP
+	            EXECUTE format('CREATE TRIGGER updated_at_trigger
+	                            BEFORE UPDATE ON %I.%I
+	                            FOR EACH ROW EXECUTE PROCEDURE %I.refresh_updated_at()',
+	                            r.table_schema, r.table_name, r.table_schema);
+	        END LOOP;
+	    END; $$ LANGUAGE plpgsql;
+	END;
+$event$ 
+LANGUAGE plpgsql;
+
+----------------------------------------------------------
+-- event trigger that calls create_trigger_after_create_event
+-- after a new table has been added to the database
+----------------------------------------------------------
+CREATE EVENT TRIGGER updated_at_event_trigger_for_after_create 
+ON ddl_command_end
+WHEN TAG IN ('CREATE TABLE')
+EXECUTE PROCEDURE create_trigger_after_create_event();
