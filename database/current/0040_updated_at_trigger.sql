183f10535cf422f7cf626590823480975086378b
Zan Peeters <zan.peeters@atventurepartners.tech>
2022-07-29 13:40:36 -0700

    Delete triggers and functions before adding; only add trigger if table has updated_at column. This prevents the error where consecutive pytest runs would error out because event triggers are not deleted at the end of the run.

--- database/current/0040_updated_at_trigger.sql
+++ database/current/0040_updated_at_trigger.sql
@@ -1,66 +1,94 @@
 ----------------------------------------------------------
--- Trigger function for updated_at column 
+-- Trigger function for updated_at column
+--
+-- This function sets the 'updated_at' column to
+-- the current timestamp on update of the row.
 ----------------------------------------------------------
-CREATE OR REPLACE function refresh_updated_at() 
-RETURNS trigger 
-LANGUAGE plpgsql AS $$
+CREATE OR REPLACE FUNCTION refresh_updated_at()
+RETURNS trigger
+AS $$
 BEGIN
-  NEW.updated_at := clock_timestamp();
-  return NEW;
+    -- NEW is an automatically created variable (type record)
+    -- that holds the new database row after update.
+    NEW.updated_at := clock_timestamp();
+    return NEW;
 END;
-$$;
+$$ LANGUAGE plpgsql;
 
 ----------------------------------------------------------
--- Sets refresh_updated_at trigger to all existing tables 
--- in the database that has the updated_at column 
+-- This PL/PGSQL code is called when this script is executed,
+-- i.e. when the database/sql_scriptrunner.py' script is run.
+--
+-- It loops over all tables in the 'public' schema that have
+-- an 'updated_at' column. It attaches a trigger to the
+-- updated_at column that calls the refresh_updated_at function.
 ----------------------------------------------------------
-
 DO $$
 DECLARE
     t record;
 BEGIN
     FOR t IN 
         SELECT * FROM information_schema.columns
-        WHERE column_name = 'updated_at' and table_schema = 'public'
+        WHERE column_name = 'updated_at' AND table_schema = 'public'
     LOOP
-        EXECUTE format('CREATE TRIGGER updated_at_trigger
-                        BEFORE UPDATE ON %I.%I
-                        FOR EACH ROW EXECUTE FUNCTION %I.refresh_updated_at()',
-                        t.table_schema, t.table_name, t.table_schema);
+        EXECUTE format(
+            'CREATE OR REPLACE TRIGGER updated_at_trigger
+            BEFORE UPDATE ON public.%I
+            FOR EACH ROW EXECUTE FUNCTION public.refresh_updated_at()',
+            t.table_name);
     END LOOP;
 END;
 $$ LANGUAGE plpgsql;
 
 ----------------------------------------------------------
--- event trigger function that loops over any tables that 
--- doesn't have an updated_at_trigger and creates the 
--- update_at_trigger for it
+-- The above code section only acts on already existing tables.
+-- Since this code is run only once in production, it does not
+-- include tables that will be created in the future.
+--
+-- This function will be called from an event trigger. Event
+-- triggers fire on DDL events, in this case CREATE TABLE.
+-- When called, this function adds the refresh_updated_at()
+-- trigger to the newly created table, if it is in the 'public'
+-- schema and has a 'updated_at' column.
 ----------------------------------------------------------
-CREATE or replace function create_trigger_after_create_event() 
-RETURNS event_trigger  AS $event$
+CREATE OR REPLACE FUNCTION add_updated_at_trigger_to_new_table()
+RETURNS event_trigger
+AS $$
+DECLARE
+    r record;
 BEGIN
-    do $$
-    DECLARE
-        r record;
-	    BEGIN
-	        FOR r IN SELECT * FROM pg_event_trigger_ddl_commands() pg 
-                     WHERE pg.object_type = 'table' and pg.schema_name = 'public'
-	        loop
-	            EXECUTE format('CREATE TRIGGER updated_at_trigger
-	                            BEFORE UPDATE ON %I
-	                            FOR EACH ROW EXECUTE FUNCTION public.refresh_updated_at()',
-	                           (parse_ident(r.object_identity))[2]);
-	        END LOOP;
-	    END; $$ LANGUAGE plpgsql;
-	END;
-$event$ 
-LANGUAGE plpgsql;
+    -- Only add if table has a updated_at column. Column names of the
+    -- created table are not available from pg_event_trigger_ddl_commands,
+    -- but luckily this function is called AFTER table creation (ddl_command_end
+    -- in the CREATE EVENT TRIGGER) so it's in the information_schema table.
+    FOR r IN
+        SELECT * FROM pg_event_trigger_ddl_commands() AS pg
+        WHERE pg.object_type = 'table'
+        AND pg.schema_name = 'public'
+        AND 'updated_at' IN (
+            SELECT column_name FROM information_schema.columns
+            WHERE ARRAY[table_schema, table_name]::text[] = parse_ident(pg.object_identity))
+    LOOP
+        EXECUTE format(
+            'CREATE OR REPLACE TRIGGER updated_at_trigger
+            BEFORE UPDATE ON %s
+            FOR EACH ROW EXECUTE FUNCTION public.refresh_updated_at()',
+            r.object_identity);
+    END LOOP;
+END;
+$$ LANGUAGE plpgsql;
 
--- ----------------------------------------------------------
--- -- event trigger that calls create_trigger_after_create_event
--- -- after a new table has been added to the database
--- ----------------------------------------------------------
-CREATE EVENT TRIGGER updated_at_event_trigger_for_after_create 
-ON ddl_command_end
-WHEN TAG IN ('CREATE TABLE')
-EXECUTE FUNCTION create_trigger_after_create_event();
+----------------------------------------------------------
+-- Create an event trigger that calls add_updated_at_trigger_to_new_table()
+-- when a new table is created.
+-- Event triggers don't have CREATE OR REPLACE, so delete old first.
+----------------------------------------------------------
+DROP EVENT TRIGGER IF EXISTS add_updated_at_trigger_to_new_table_event_trigger;
+CREATE EVENT TRIGGER add_updated_at_trigger_to_new_table_event_trigger
+    ON ddl_command_end
+    WHEN TAG IN ('CREATE TABLE')
+    EXECUTE FUNCTION add_updated_at_trigger_to_new_table();
+
+-- Drop old names
+DROP EVENT TRIGGER IF EXISTS updated_at_event_trigger_for_after_create;
+DROP FUNCTION IF EXISTS create_trigger_after_create_event;
