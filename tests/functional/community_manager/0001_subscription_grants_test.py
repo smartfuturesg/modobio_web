eb3c0d3fc93fa19a46b84c3f4db5079633330cd7
Alejandro <alejandro.lorenzo@atventurepartners.tech>
2022-09-06 16:51:44 -0700

    fixture for unverified user, test for unverified user subscription grant :

--- tests/functional/community_manager/0001_subscription_grants_test.py
+++ tests/functional/community_manager/0001_subscription_grants_test.py
@@ -4,7 +4,12 @@ from odyssey.api.community_manager.models import CommunityManagerSubscriptionGra
 from odyssey.api.user.models import UserSubscriptions
 
 
+
 def test_post_subscription_grant(test_client):
+    """
+    POST subscription grants for user by email and modobio_id. Also POST subscription grant for non-user
+    
+    """
     data = {
         "emails": ["client@modobio.com", "not_a_user@gmail.com"],
         "modobio_ids": ["TC12JASDFF12"],
@@ -50,6 +55,40 @@ def test_post_subscription_grant_bad_modo_id(test_client):
     )
     
     assert response.status_code == 400
+
+def test_post_subscription_grant_unverified_user(test_client, unverified_user):
+    """
+    Test adding subscription grant for an unverified user. 
+
+    Subscription grant should be stored and the user's subscription state should not be updated
+    
+    """
+    # post grant for user
+    data = {
+        "emails": [unverified_user.email],
+        "modobio_ids": [],
+        "sponsor": "Sponsoring Institution",
+        "subscription_type_id": 2,
+    }
+
+    response = test_client.post(
+        f"/community-manager/subscriptions/grant",
+        headers=test_client.staff_auth_header,
+        data=dumps(data),
+        content_type="application/json",
+    )
+
+    # bring up subscriptions to verify nothing has been updated
+    subscriptions = UserSubscriptions.query.filter_by(user_id = unverified_user.user_id).all()
+    grants = CommunityManagerSubscriptionGrants.query.filter_by(
+        subscription_grantee_user_id=unverified_user.user_id
+    ).all()
+
+    assert response.status_code == 200
+    assert len(subscriptions) == 1
+    assert len(grants) == 1
+
+    # verify user and check that subscription has been updated
 
 
 def test_get_subscriptions_granted(test_client, subscription_grants):
--- tests/functional/community_manager/conftest.py
+++ tests/functional/community_manager/conftest.py
@@ -1,6 +1,8 @@
+from re import U
 import pytest
 
 from odyssey.api.community_manager.models import CommunityManagerSubscriptionGrants
+from odyssey.api.user.models import User, UserSubscriptions
 
 @pytest.fixture(scope="function")
 def subscription_grants(test_client):
@@ -30,4 +32,29 @@ def subscription_grants(test_client):
 
     # delete all subscription grants
     test_client.db.session.query(CommunityManagerSubscriptionGrants).delete()
+    test_client.db.session.commit()
+
+@pytest.fixture(scope="function")
+def unverified_user(test_client):
+    """
+    Creates a new client user that is not verified
+    """
+    user = User(email = "unverified_test_user@modo.com", firstname = "Testy", lastname = "test", phone_number = '1231231434', is_staff=False, was_staff=False)
+    test_client.db.session.add(user)
+    test_client.db.session.flush()
+    user_id = user.user_id
+    user_subscription = UserSubscriptions(
+            subscription_status = 'unsubscribed',
+            is_staff= False,
+            user_id = user.user_id
+        )
+    
+    test_client.db.session.add(user_subscription)
+    test_client.db.session.commit()
+
+
+    yield user
+    
+    User.query.filter_by(user_id = user_id).delete()
+    UserSubscriptions.query.filter_by(user_id = user_id).delete()
     test_client.db.session.commit()
