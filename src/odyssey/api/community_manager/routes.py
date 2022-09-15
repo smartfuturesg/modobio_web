d5f80c98a5a88a5aa692efcd850e638f3a4dad1a
Samson Bankole <samson.bankole@atventurepartners.tech>
2022-09-15 11:24:20 -0500

    Community manager medical credentials verification

--- src/odyssey/api/community_manager/routes.py
+++ src/odyssey/api/community_manager/routes.py
@@ -1,16 +1,24 @@
+from datetime import datetime, timedelta
 import logging
 
-from flask import current_app, request
+from flask import current_app, request, url_for
 from flask_accepts import accepts, responds
 from flask_restx import Namespace
 from werkzeug.exceptions import BadRequest
 
 from odyssey import db
 from odyssey.api.community_manager.models import CommunityManagerSubscriptionGrants
-from odyssey.api.community_manager.schemas import PostSubscriptionGrantSchema, SubscriptionGrantSchema, SubscriptionGrantsAllSchema
+from odyssey.api.community_manager.schemas import (
+    PostSubscriptionGrantSchema, 
+    ProviderLiscensingAllSchema, 
+    VerifyMedicalCredentialSchema, 
+    SubscriptionGrantsAllSchema, 
+    ProviderLicensingSchema
+)
 from odyssey.api.lookup.models import LookupSubscriptions
 
 from odyssey.api.user.models import User
+from odyssey.api.practitioner.models import PractitionerCredentials
 from odyssey.tasks.tasks import update_client_subscription_task
 from odyssey.utils.auth import token_auth
 from odyssey.utils.base.resources import BaseResource
@@ -137,3 +145,129 @@ class CMSubscriptionGrantingEndpoint(BaseResource):
         db.session.commit()
 
         return 
+
+@ns.route("/provider-licensing")
+class CMProviderLicensingEndpoint(BaseResource):
+    """Provider Licensing Endpoint"""
+    
+    @token_auth.login_required(user_type=("staff",), staff_role=("community_manager",))
+    @responds(schema=ProviderLiscensingAllSchema, api=ns)
+    @ns.doc(params={'status': 'Verification Status',
+                    'email': 'Email Address',
+                    'modobio_id': 'Modo Bio ID',
+                    'start_date': 'Start date range of Query', 
+                    'end_date': 'End date range of Query', 
+                    'page': 'Pagination Index', 
+                    'per_page': 'Results per page'
+    })
+    def get(self):
+        """
+        Gets all medical credential licenses in the system
+
+        Returns
+            -------
+            dict
+                Medical Credentials Info Per User .
+        """
+
+        status = request.args.get('status', type=str)
+        email = request.args.get('email', type=str)
+        modobio_id = request.args.get('modobio_id', type=str)
+        start_date = request.args.get('start_date', type=str)
+        end_date = request.args.get('end_date', type=str)
+        page = request.args.get('page', 1, type=int)
+        per_page = request.args.get('per_page', 100, type=int)
+
+        query = db.session.query(PractitionerCredentials, User)\
+            .join(User, User.user_id == PractitionerCredentials.user_id)
+
+        if status:
+            query = query.filter(PractitionerCredentials.status == status)
+
+            if status == 'Pending Verification':
+                query = query.order_by(PractitionerCredentials.created_at.asc())
+            if status == 'Verified':
+                query = query.order_by(PractitionerCredentials.updated_at.asc())
+            if status == 'Rejected':
+                query = query.order_by(PractitionerCredentials.created_at.asc())
+            if status == 'Expired':
+                query = query.order_by(PractitionerCredentials.expiration_date.asc())
+
+        if email:
+            query = query.filter(User.email == email)
+        if modobio_id:
+            query = query.filter(User.modobio_id == modobio_id)
+        
+        # Convert strings to datetimes
+        if start_date:
+            start_date = datetime.strptime(start_date, '%Y-%m-%d')
+            if start_date > datetime.utcnow():
+                raise BadRequest('Start date can not be in the future.')
+
+        if end_date:
+            end_date = datetime.strptime(end_date, '%Y-%m-%d')
+
+        if start_date and end_date:
+            if (end_date - start_date).days > 90:
+                raise BadRequest('Date Range Limited to 90 Days.')
+            query = query.filter(PractitionerCredentials.created_at.between(start_date, end_date))
+        if start_date and not end_date:
+            end_date = start_date + timedelta(days=90)
+            query = query.filter(PractitionerCredentials.created_at.between(start_date, end_date))
+        if end_date and not start_date:
+            start_date = end_date - timedelta(days=90)
+            query = query.filter(PractitionerCredentials.created_at.between(start_date, end_date))
+
+        pagination_query = query.paginate(page, per_page, error_out=False)
+        items = pagination_query.items
+        result = []
+        for cred, user in items:
+            cred.firstname = user.firstname
+            cred.lastname = user.lastname
+            cred.modobio_id = user.modobio_id
+            result.append(cred)
+
+        return {
+            'provider_licenses': result, 
+            'total_items': pagination_query.total,
+            '_links': {
+                '_prev': url_for('api.community-manager_cm_provider_licensing_endpoint', page=pagination_query.prev_num, per_page = per_page) if pagination_query.has_prev else None,
+                '_next': url_for('api.community-manager_cm_provider_licensing_endpoint', page=pagination_query.next_num, per_page = per_page) if pagination_query.has_next else None,
+                }
+            }
+
+@ns.route("/verify-credentials")
+class CMVerifyCredentialsEndpoint(BaseResource):
+    """Verify Medical Credentials Endpoint"""
+
+    @token_auth.login_required(user_type=("staff",), staff_role=("community_manager",))
+    @accepts(schema=VerifyMedicalCredentialSchema, api=ns)
+    @responds(schema=ProviderLicensingSchema(exclude=('created_at', 'updated_at')), status_code=200,api=ns)
+    def put(self):
+        """
+        PUT Request for updating the status for medical credentials
+        """
+
+        payload = request.json
+        
+        #Validate Status
+        status = ['Verified','Pending Verification', 'Rejected', 'Expired']
+        if payload['status'] not in status:
+            raise BadRequest('Status must be one of {}.'.format(', '.join(status)))
+
+        query = db.session.query(PractitionerCredentials, User)\
+            .join(User, User.user_id == PractitionerCredentials.user_id).\
+                filter(PractitionerCredentials.user_id==payload['user_id']\
+                    ,PractitionerCredentials.idx==payload['idx']).one_or_none()
+
+        if query: #query = (<PractitionerCredentials>, <User>)
+            query[0].update(payload)
+            db.session.commit()
+        else:
+            raise BadRequest('Credentials not found.')
+
+        #Include modobio id in response payload if status is markes as Rejected or Verified
+        if payload['status'] == 'Rejected' or payload['status'] == 'Verified':
+            query[0].modobio_id = query[1].modobio_id
+
+        return query[0]
