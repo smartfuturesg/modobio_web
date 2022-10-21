7f090002e772a9b49f339d8cfe8eb2c197ae048a
Samson Bankole <samson.bankole@atventurepartners.tech>
2022-10-21 15:49:55 -0500

    Update test

--- tests/functional/practitioner/1013_telehealth_access_test.py
+++ tests/functional/practitioner/1013_telehealth_access_test.py
@@ -3,27 +3,34 @@ from odyssey.api.user.models import User
 
 
 def test_get_telehealth_activation(test_client):
+    user = User.query.filter_by(user_id=test_client.staff_id).one_or_none()
+
     response = test_client.get(
         f'/practitioner/telehealth-activation/{test_client.staff_id}/',
         headers=test_client.staff_auth_header,
         content_type='application/json')
 
-    assert response.json['provider_telehealth_access'] == False
+    assert response.json['provider_telehealth_access'] == user.provider_telehealth_access
     assert response.status_code == 200
 
 def test_update_telehealth_activation(test_client):
-    data = {"provider_telehealth_access" : True}
+    user = User.query.filter_by(user_id=test_client.staff_id).one_or_none()
+
+    current_telehealth_access_flag = user.provider_telehealth_access
+
+    #switch telehealth flag
+    data = {"provider_telehealth_access" : not current_telehealth_access_flag}
     response = test_client.put(
         f'/practitioner/telehealth-activation/{test_client.staff_id}/',
         headers=test_client.staff_auth_header,
         data=dumps(data),
         content_type='application/json')
     assert response.status_code == 200
-    assert response.json['provider_telehealth_access'] == True
+    assert response.json['provider_telehealth_access'] !=  current_telehealth_access_flag
 
-    user = User.query.filter_by(user_id=test_client.staff_id).one_or_none()
-    assert user.provider_telehealth_access == True
-
-    #set flag back to false
-    user.provider_telehealth_access = False
+    #Restore flag
+    user.provider_telehealth_access = current_telehealth_access_flag
     test_client.db.session.commit()
+    assert user.provider_telehealth_access == current_telehealth_access_flag
+
+ 
