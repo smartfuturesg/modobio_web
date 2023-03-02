1236b04a0d0c3b3aa099de3aa0b461165babc399
Zan Peeters <zan.peeters@atventurepartners.tech>
2023-03-02 13:20:27 -0700

    Move TELEHEALTH_BOOKING_LEAD_TIME_HRS, TELEHEALTH_BOOKING_TRANSCRIPT_EXPIRATION_HRS, TELEHEALTH_BOOKING_DURATION, and WEARABLE_DATA_DEFAULT_RANGE_DAYS from constants.py to defaults.py. They are configurable variables rather than constants and the first two in fact do change depending on the environment.

--- src/odyssey/tasks/periodic.py
+++ src/odyssey/tasks/periodic.py
@@ -27,7 +27,7 @@ from odyssey.api.client.models import ClientClinicalCareTeamAuthorizations, Clie
 from odyssey.api.lookup.models import LookupBookingTimeIncrements
 from odyssey.api.notifications.schemas import NotificationSchema
 from odyssey.api.user.models import User, UserSubscriptions
-from odyssey.utils.constants import TELEHEALTH_BOOKING_TRANSCRIPT_EXPIRATION_HRS, NOTIFICATION_SEVERITY_TO_ID, NOTIFICATION_TYPE_TO_ID
+from odyssey.utils.constants import NOTIFICATION_SEVERITY_TO_ID, NOTIFICATION_TYPE_TO_ID
 from odyssey.utils.misc import get_time_index, create_notification
 from odyssey.tasks.tasks import cancel_telehealth_appointment
 from odyssey.utils.message import send_email
