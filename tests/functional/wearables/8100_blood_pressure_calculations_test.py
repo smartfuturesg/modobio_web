d0534d5c5dc6c6c034c28a6279df6c2728e8c3a0
trevor-stahl <trevor.stahl@atventurepartners.tech>
2023-03-22 16:34:55 -0500

    fix route typo in second pytest, remove skip, uncomment various sections and use new excel calculations for asserts

--- tests/functional/wearables/8100_blood_pressure_calculations_test.py
+++ tests/functional/wearables/8100_blood_pressure_calculations_test.py
@@ -26,10 +26,10 @@ def test_blood_pressure_calculations_default_date_filters(test_client, bp_data_f
     # breakpoint()
 
 
-@pytest.mark.skip('DEV')
+# @pytest.mark.skip('DEV')
 def test_blood_pressure_calculations_extended_date_filters(test_client, bp_data_fixture):
     response = test_client.get(
-        f'/v2/wearables/calculations/blood-pressure/variation/{test_client.client_id}/{BLOOD_PRESSURE_WEARABLE}?start_date{datetime.utcnow() - timedelta(weeks=40)}=&end_date={datetime.utcnow() + timedelta(weeks=1)}',
+        f'/v2/wearables/calculations/blood-pressure/variation/{test_client.client_id}/{BLOOD_PRESSURE_WEARABLE}?start_date={datetime.utcnow() - timedelta(weeks=40)}&end_date={datetime.utcnow() + timedelta(weeks=1)}',
         headers=test_client.staff_auth_header,
         content_type='application/json',
     )
@@ -37,10 +37,10 @@ def test_blood_pressure_calculations_extended_date_filters(test_client, bp_data_
     assert response.status_code == 200
     assert response.json.get('user_id') == test_client.client_id
     assert response.json.get('wearable') == BLOOD_PRESSURE_WEARABLE
-    assert response.json.get('diastolic_bp_avg') == 72
-    assert response.json.get('systolic_bp_avg') == 131
-    # assert response.json.get('diastolic_standard_deviation') == 4
-    # assert response.json.get('systolic_standard_deviation') == 10
+    assert response.json.get('diastolic_bp_avg') == 76
+    assert response.json.get('systolic_bp_avg') == 129
+    assert response.json.get('diastolic_standard_deviation') == 11
+    assert response.json.get('systolic_standard_deviation') == 13
     # assert response.json.get('diastolic_bp_coefficient_of_variation') is None
     # assert response.json.get('systolic_bp_coefficient_of_variation') is None
 
