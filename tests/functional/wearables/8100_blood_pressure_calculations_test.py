8b527d5bf6b07c8e177c98a688373dae9edf7db0
trevor-stahl <trevor.stahl@atventurepartners.tech>
2023-03-22 16:17:28 -0500

    refactor to use wearables conftest.py fixture

--- tests/functional/wearables/8100_blood_pressure_calculations_test.py
+++ tests/functional/wearables/8100_blood_pressure_calculations_test.py
@@ -1,3 +1,4 @@
+from datetime import datetime, timedelta
 import uuid
 import pytest
 
@@ -5,25 +6,7 @@ from odyssey.api.wearables.models import WearablesV2
 from .data import BLOOD_PRESSURE_WEARABLE, test_8100_data_past_week, test_8100_data_week_to_month_ago
 
 
-def test_blood_pressure_calculations_default_date_filters(test_client):
-    # insert test data
-    # I am not using the store_data function in the TerraClient 'tc', for now
-    # Instead I am attempting to inject data in the format that function would upsert it in
-    # This could need to be changed someday to use that function plus datetime now instead of hard coded test data
-    # However, that would require a bit of work to figure out what exactly we pass to that function or refactoring tc
-    test_8100_data_past_week['user_id'] = test_client.client_id
-    test_8100_data_week_to_month_ago['user_id'] = test_client.client_id
-    test_client.mongo.db.wearables.insert_many([test_8100_data_past_week, test_8100_data_week_to_month_ago])
-
-    w = WearablesV2(
-        wearable=BLOOD_PRESSURE_WEARABLE,
-        terra_user_id=uuid.uuid4(),
-        user_id=test_client.client_id,
-    )
-    test_client.db.session.add(w)
-    test_client.db.session.commit()
-
-    # call endpoint
+def test_blood_pressure_calculations_default_date_filters(test_client, bp_data_fixture):
     response = test_client.get(
         f'/v2/wearables/calculations/blood-pressure/variation/{test_client.client_id}/{BLOOD_PRESSURE_WEARABLE}',
         headers=test_client.staff_auth_header,
@@ -40,33 +23,13 @@ def test_blood_pressure_calculations_default_date_filters(test_client):
     # assert response.json.get('diastolic_bp_coefficient_of_variation') is None
     # assert response.json.get('systolic_bp_coefficient_of_variation') is None
 
-    # clean up mongoDB
-    del_query = {'user_id': test_client.client_id, 'wearable': BLOOD_PRESSURE_WEARABLE}
-    test_client.mongo.db.wearables.delete_many(del_query)
-
     # breakpoint()
 
 
 @pytest.mark.skip('DEV')
-def test_blood_pressure_calculations_extended_date_filters(test_client):
-    # test_8100_data_past_week['user_id'] = test_client.client_id
-    # test_8100_data_week_to_month_ago['user_id'] = test_client.client_id
-    # test_client.mongo.db.wearables.insert_many([test_8100_data_past_week, test_8100_data_week_to_month_ago])
-    #
-    # w = WearablesV2(
-    #     wearable=BLOOD_PRESSURE_WEARABLE,
-    #     terra_user_id=uuid.uuid4(),
-    #     user_id=test_client.client_id,
-    # )
-    # test_client.db.session.add(w)
-    # test_client.db.session.commit()
-
-    future = '2040-01-01'
-    past = '2020-01-01'
-
-    # call endpoint
+def test_blood_pressure_calculations_extended_date_filters(test_client, bp_data_fixture):
     response = test_client.get(
-        f'/v2/wearables/calculations/blood-pressure/variation/{test_client.client_id}/{BLOOD_PRESSURE_WEARABLE}?start_date{past}=&end_date={future}',
+        f'/v2/wearables/calculations/blood-pressure/variation/{test_client.client_id}/{BLOOD_PRESSURE_WEARABLE}?start_date{datetime.utcnow() - timedelta(weeks=40)}=&end_date={datetime.utcnow() + timedelta(weeks=1)}',
         headers=test_client.staff_auth_header,
         content_type='application/json',
     )
@@ -80,9 +43,5 @@ def test_blood_pressure_calculations_extended_date_filters(test_client):
     # assert response.json.get('systolic_standard_deviation') == 10
     # assert response.json.get('diastolic_bp_coefficient_of_variation') is None
     # assert response.json.get('systolic_bp_coefficient_of_variation') is None
-
-    # clean up mongoDB
-    del_query = {'user_id': test_client.client_id, 'wearable': BLOOD_PRESSURE_WEARABLE}
-    test_client.mongo.db.wearables.delete_many(del_query)
 
     # breakpoint()
--- tests/functional/wearables/conftest.py
+++ tests/functional/wearables/conftest.py
@@ -1,6 +1,11 @@
+import uuid
+
 import pytest
 
-from .data import blood_glucose_data_1, blood_glucose_data_2, BLOOD_GLUCOSE_WEARABLE
+from odyssey.api.wearables.models import WearablesV2
+from .data import blood_glucose_data_1, blood_glucose_data_2, BLOOD_GLUCOSE_WEARABLE, test_8100_data_past_week, \
+    test_8100_data_week_to_month_ago, BLOOD_PRESSURE_WEARABLE
+
 
 @pytest.fixture(scope='function')
 def add_blood_glucose_data(test_client):
@@ -13,3 +18,26 @@ def add_blood_glucose_data(test_client):
 
     del_query = {'user_id': test_client.client_id, 'wearable': BLOOD_GLUCOSE_WEARABLE}
     test_client.mongo.db.wearables.delete_many(del_query)
+
+
+@pytest.fixture(scope='function')
+def bp_data_fixture(test_client):
+    test_8100_data_past_week['user_id'] = test_client.client_id
+    test_8100_data_week_to_month_ago['user_id'] = test_client.client_id
+    test_client.mongo.db.wearables.insert_many([test_8100_data_past_week, test_8100_data_week_to_month_ago])
+
+    # w = WearablesV2(
+    #     wearable=BLOOD_PRESSURE_WEARABLE,
+    #     terra_user_id=uuid.uuid4(),
+    #     user_id=test_client.client_id,
+    # )
+    # test_client.db.session.add(w)
+    # test_client.db.session.commit()
+
+    yield [test_8100_data_past_week, test_8100_data_week_to_month_ago]
+
+    del_query = {'user_id': test_client.client_id, 'wearable': BLOOD_PRESSURE_WEARABLE}
+    test_client.mongo.db.wearables.delete_many(del_query)
+
+    # test_client.db.session.delete(w)
+    # test_client.db.session.commit()
