34af76bd84c4734b5101fb1562d16e5ff4792d83
trevor-stahl <trevor.stahl@atventurepartners.tech>
2023-04-10 02:45:02 -0500

    remove unused pytest skip, add in more comments

--- tests/functional/integrations/5000_terra_store_data_test.py
+++ tests/functional/integrations/5000_terra_store_data_test.py
@@ -1,5 +1,4 @@
 import requests
-# import pytest
 
 from flask import current_app
 from terra.api.api_responses import TerraApiResponse
@@ -8,7 +7,6 @@ from odyssey.api.wearables.models import WearablesV2
 from odyssey.integrations.terra import TerraClient
 
 
-# @pytest.mark.skip('IN DEV')
 def test_store_data(test_client):
     # test the TerraClient() class instantiation
     tc = TerraClient()
@@ -18,15 +16,16 @@ def test_store_data(test_client):
     dev_id = current_app.config['TERRA_DEV_ID']
     x_api_key = current_app.config['TERRA_API_KEY']
 
+    # get basically a list of users from terra
     headers = {
         'dev-id': dev_id,
         'x-api-key': x_api_key,
     }
-
     response = requests.get(url='https://api.tryterra.co/v2/subscriptions', headers=headers).json()
 
     assert response
 
+    # try to get the terra user id for someone that uses OURA
     terra_user_id = None
 
     for u in response['users']:
@@ -34,15 +33,20 @@ def test_store_data(test_client):
             terra_user_id = u['user_id']
             break
 
+    # check we got one
     assert terra_user_id is not None
 
+    # build url for getting body for user
     url = 'https://api.tryterra.co/v2/body?user_id=' + str(terra_user_id)
     url += '&start_date=2022-06-01&end_date=2022-06-27&to_webhook=false&with_samples=true'
 
+    # send terra the get request
     response = requests.get(url=url, headers=headers)
 
+    # create a terra library object that can be passed into out own client
     terra_response = TerraApiResponse(resp=response, dtype='activity')
 
+    # we need to inject into Wearables
     user_wearable = WearablesV2(
         user_id=test_client.client_id,
         wearable='OURA',
@@ -51,20 +55,24 @@ def test_store_data(test_client):
     test_client.db.session.add(user_wearable)
     test_client.db.session.commit()
 
+    # track the current count in case someone does not clean up after themselves down the road
     before_mongo_count = test_client.mongo.db.wearables.count_documents({})
 
+    # use the terra client store_data function which is the center point of our data intake
     tc.store_data(terra_response)
 
+    # record the new document count for wearables collection
     after_mongo_count = test_client.mongo.db.wearables.count_documents({})
 
+    # assert to check that store_data worked
     assert 0 <= before_mongo_count < after_mongo_count
 
     # clean up
     test_client.mongo.db.wearables.delete_many({
         'user_id': test_client.client_id
     })
-
-    assert test_client.mongo.db.wearables.count_documents({}) == before_mongo_count
-
     test_client.db.session.delete(user_wearable)
     test_client.db.session.commit()
+
+    # check that we are back to where we started to try to keep idempotency
+    assert test_client.mongo.db.wearables.count_documents({}) == before_mongo_count
