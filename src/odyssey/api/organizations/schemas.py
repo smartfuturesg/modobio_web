38abca2af986c0999f4f3f0c6d4eb7d9c9aa63c9
trevor-stahl <trevor.stahl@atventurepartners.tech>
2023-06-09 13:45:23 -0500

    fix schema, needs owner and must load

--- src/odyssey/api/organizations/schemas.py
+++ src/odyssey/api/organizations/schemas.py
@@ -8,7 +8,9 @@ class OrganizationsSchema(ma.SQLAlchemyAutoSchema):
     class Meta:
         model = Organizations
         dump_only = ('organization_id',)
+        load_instance = True
 
     name = fields.String(required=True, validate=validate.Length(min=3, max=100))
     max_members = fields.Integer(required=True, validate=validate.Range(min=1, max=10000))
     max_admins = fields.Integer(required=True, validate=validate.Range(min=1, max=10000))
+    owner = fields.Integer(required=True)
