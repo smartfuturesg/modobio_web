e5ac1f2704b8497265fdda23f6362bc8f4e9f9b5
trevor-stahl <trevor.stahl@atventurepartners.tech>
2023-06-09 12:38:07 -0500

    use a marshmallow simple Schema instead of an autoschema for orgs

--- src/odyssey/api/organizations/schemas.py
+++ src/odyssey/api/organizations/schemas.py
@@ -1,16 +1,14 @@
-from marshmallow import fields, validate
-
-from odyssey import ma
+from marshmallow import fields, validate, Schema
 
 
-class PostOrganizationInputSchema(ma.SQLAlchemyAutoSchema):
+class PostOrganizationInputSchema(Schema):
     name = fields.String(required=True, validate=validate.Length(min=3, max=100))
     max_members = fields.Integer(required=True, validate=validate.Range(min=1, max=10000))
     max_admins = fields.Integer(required=True, validate=validate.Range(min=1, max=10000))
     owner = fields.Integer(required=True)
 
 
-class PostOrganizationOutputSchema(ma.SQLAlchemyAutoSchema):
+class PostOrganizationOutputSchema(Schema):
     organization_id = fields.Integer()
     name = fields.String()
     max_members = fields.Integer()
