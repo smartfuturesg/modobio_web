c3b3787f9567862a6fa00f7928cb692f3247b15c
Alejandro <alejandro.lorenzo@atventurepartners.tech>
2023-06-16 12:50:45 -0700

    move script

--- database/current/0001_LookupSubscriptions_google_product_id.sql
+++
@@ -1,7 +0,0 @@
-update "LookupSubscriptions"
-set google_product_id = 'mb_annual_ars_97.99_01'
-where name = 'Annual';
-
-update "LookupSubscriptions"
-set google_product_id = 'mb_monthly_ars_9.99_01'
-where name = 'Monthly';
