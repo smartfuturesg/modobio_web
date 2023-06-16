87b90f9aff4971dfb5a3dbd8f5f838307cbdfd53
trevor-stahl <trevor.stahl@atventurepartners.tech>
2023-06-16 16:34:38 -0500

    add new tests for invalid org creations

--- tests/functional/organizations/0050_crud_orgs_test.py
+++ tests/functional/organizations/0050_crud_orgs_test.py
@@ -1,5 +1,6 @@
 from flask.json import dumps
 
+from odyssey.api.organizations.models import Organizations
 from .data import org
 
 
@@ -42,3 +43,71 @@ def test_create_org_with_existing_name(test_client):
         content_type='application/json')
 
     assert response.status_code == 400
+
+
+def test_create_org_with_invalid_owner(test_client):
+    """ Test that a community manager can NOT create an organization with an invalid owner. """
+    bad_owner_org = org.copy()
+    bad_owner_org['owner'] = 0
+
+    response = test_client.post(
+        '/organizations/',
+        headers=test_client.staff_auth_header,
+        data=dumps(bad_owner_org),
+        content_type='application/json'
+    )
+
+    assert response.status_code == 400
+
+
+def test_create_org_with_invalid_max_members(test_client):
+    """ Test that a community manager can NOT create an organization with an invalid max_members. """
+    bad_max_members_org = org.copy()
+    bad_max_members_org['max_members'] = 1000000000
+
+    response = test_client.post(
+        '/organizations/',
+        headers=test_client.staff_auth_header,
+        data=dumps(bad_max_members_org),
+        content_type='application/json'
+    )
+
+    assert response.status_code == 400
+
+
+def test_create_org_with_invalid_max_admins(test_client):
+    """ Test that a community manager can NOT create an organization with an invalid max_admins. """
+    bad_max_admins_org = org.copy()
+    bad_max_admins_org['max_admins'] = 1000000000
+
+    response = test_client.post(
+        '/organizations/',
+        headers=test_client.staff_auth_header,
+        data=dumps(bad_max_admins_org),
+        content_type='application/json'
+    )
+
+    assert response.status_code == 400
+
+
+# def test_add_members_to_org(test_client):
+#     """ Test that a community manager can add members to a valid organization. """
+#     # Query for the organization_uuid
+#     o = Organizations.query.filter_by(name=org['name']).first()
+#     org_uuid = o.organization_uuid
+#
+#     response = test_client.post(
+#         '/organizations/members/',
+#         headers=test_client.staff_auth_header,
+#         data=dumps({
+#             'organization_uuid': org_uuid,
+#             'members': [0, 17, 1, 2, 3]
+#         }),
+#         content_type='application/json'
+#     )
+#
+#     assert response.status_code == 201
+#     assert response.json['organization_uuid'] == str(org_uuid)
+#     assert response.json['added_members'] == [1, 2, 3]
+#     assert response.json['invalid_members'] == [0]
+#     assert response.json['prior_members'] == [17]
