6688819366362176331569021544ffe7e9482e35
trevor-stahl <trevor.stahl@atventurepartners.tech>
2023-06-16 19:49:50 -0500

    not sure why but new relations required minor schema fix

--- src/odyssey/api/organizations/schemas.py
+++ src/odyssey/api/organizations/schemas.py
@@ -7,13 +7,14 @@ from odyssey.api.organizations.models import Organizations
 class OrganizationsSchema(ma.SQLAlchemyAutoSchema):
     class Meta:
         model = Organizations
-        dump_only = ('organization_uuid', )
+        exclude = ('created_at', 'updated_at')
         load_instance = True
 
     name = fields.String(required=True, validate=validate.Length(min=3, max=100))
     max_members = fields.Integer(required=True, validate=validate.Range(min=1, max=10000))
     max_admins = fields.Integer(required=True, validate=validate.Range(min=1, max=10000))
     owner = fields.Integer(required=True)
+    organization_uuid = fields.UUID(dump_only=True)
 
 
 class OrganizationMembersPostInputSchema(Schema):
