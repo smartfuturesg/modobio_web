5a04f88935a6282b6da2d566788f191e0b014384
Alejandro <alejandro.lorenzo@atventurepartners.tech>
2023-08-18 07:45:31 -0700

    typo

--- src/odyssey/api/community_manager/routes.py
+++ src/odyssey/api/community_manager/routes.py
@@ -143,7 +143,7 @@ class CMSubscriptionGrantingEndpoint(BaseResource):
         user_ids = [uid for uid in user_ids if uid not in unverified_user_ids]
         for user_id in user_ids:
             if current_app.testing:
-                update_client_subscription(user_id, auto_renew=False)
+                update_client_subscription(user_id)
             else:
                 update_client_subscription_task.delay(user_id, auto_renew=False)
 
