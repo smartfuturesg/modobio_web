6cae1caabe486e6af6f8e20dcee93538cc43c2fa
alorenzo <alejandro.lorenzo@atventurepartners.tech>
2023-10-02 11:40:55 -0700

    bp get data test

---
+++ tests/functional/wearables/9001_get_bp_data_test.py
@@ -0,0 +1,32 @@
+from datetime import datetime, timedelta
+import json
+
+
+def test_get_bp_data(test_client, add_blood_pressure_data):
+    """Returns blood pressure data from all sources"""
+
+    response = test_client.get(
+        f"/v2/wearables/data/blood-pressure/{test_client.client_id}",
+        headers=test_client.provider_auth_header,
+        content_type="application/json",
+    )
+
+    data = response.json
+
+    assert response.status_code == 200
+    assert data.get("total_items") == 2
+
+
+def test_get_bp_data_with_dates(test_client, add_blood_pressure_data):
+    start_date = datetime.utcnow() - timedelta(weeks=3)
+    end_date = datetime.utcnow() - timedelta(weeks=2)
+    response = test_client.get(
+        f"/v2/wearables/data/blood-pressure/{test_client.client_id}?start_date={start_date.isoformat()}&end_date={end_date.isoformat()}",
+        headers=test_client.provider_auth_header,
+        content_type="application/json",
+    )
+
+    data = response.json
+
+    assert response.status_code == 200
+    assert data.get("total_items") == 0
